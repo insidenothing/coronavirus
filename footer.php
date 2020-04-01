800baba9b381a0cd7d6af29d889bc2df27aa7ddb
Patrick McGuire <insidenothing@gmail.com>
2020-04-01 13:28:15 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
   <br><img src='flag.jpg'><br>
   &copy 2020 <a target='_Blank' href='http://sales.patrickmcguire.me/'>Patrick McGuire</a> 
-| <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a>
+| <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a> | <a target='_Blank' href='https://github.com/insidenothing/coronavirus'>Open Source - View at Github.com</a>
 </div>
 </center>
 </body>
