5ee7dcaba9a122b9a1fbaf9d06a0b736768202a3
Patrick McGuire <insidenothing@gmail.com>
2020-04-02 12:55:56 -0400

    Update secure.php

--- secure.php
+++ secure.php
@@ -5,4 +5,7 @@ $user = '';
 $pass = '';
 $db_name = '';
 $core = mysqli_connect($host,$user,$pass,$db_name) or die("Error " . mysqli_error($core));
+global $twillo_account;
+global $twillo_key;
 $twillo_key = '';
+$twillo_account = '';
