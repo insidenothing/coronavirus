db2d20fa9688d96856d8dc4b588b12c4d2623d19
Patrick McGuire <insidenothing@gmail.com>
2020-04-03 08:14:43 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if(empty($_GET['novideo'])){
 function coronavirus_levels($new_id,$name,$state,$Cases,$Deaths,$Recovered){
 	global $core;
 	$core->query("insert into coronavirus_levels ( update_id, populations_name, pouplations_state, checked_datetime, just_date, Cases, Deaths, Recovered)
-	values ('$new_id', '$name', now(), now(), '$Cases', '$Deaths', '$Recovered' ) ");
+	values ('$new_id', '$name', '$state', NOW(), NOW(), '$Cases', '$Deaths', '$Recovered' ) ");
 	global $current_total_cases2;
 	$current_total_cases2 = $current_total_cases2 + $Cases;
 	global $current_total_deaths2;
