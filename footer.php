b5fe22d535d40e34dcddfdedb0297e612baa2d45
Patrick McGuire <insidenothing@gmail.com>
2020-04-03 15:42:39 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 
-  <br><img src='flag.jpg'><br>
+  <br><img src='img/flag.jpg'><br>
   &copy 2020 <a target='_Blank' href='http://sales.patrickmcguire.me/'>Patrick McGuire</a> 
 | <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a> | <a target='_Blank' href='https://github.com/insidenothing/coronavirus'>Open Source - View at Github.com</a>
 </div>
