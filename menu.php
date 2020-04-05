38ba6ffa33dc7319ef2caa9ea352bd42196dd770
Patrick McGuire <insidenothing@gmail.com>
2020-04-05 16:47:50 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -330,15 +330,14 @@ return $return;
 	}
 	?>
 <table><tr>
-	<td><a href='index.php'><img src='img/home.png'></a></td>
-	<td><a href='graphs.php'><img src='img/graph_county.png'></a></td>
-	<td><a href='graph_age.php'><img src='img/graph_age.png'></a></td>
-	<td><a href='graph_hospital.php'><img src='img/graph_hospital.png'></a></td>
-	<td><a href='graph_delta.php'><img src='img/graph_delta.png'></a></td>
-	<td><a href='infection_level.php'><img src='img/infected.png'></a></td>
-	<td><a href='signup.php'><img src='img/signup.png'></a></td>
-	<td><a href='https://www.facebook.com/groups/231583938033989/'><img src='img/facebook.png'></a></td>
-	<td><a href='wiki.php'><img src='img/recovered.png'></a></td>
+	<td><a href='index.php'><img class="img-responsive" src='img/home.png'></a></td>
+	<td><a href='graphs.php'><img class="img-responsive" src='img/graph_county.png'></a></td>
+	<td><a href='graph_age.php'><img class="img-responsive" src='img/graph_age.png'></a></td>
+	<td><a href='graph_hospital.php'><img class="img-responsive" src='img/graph_hospital.png'></a></td>
+	<td><a href='graph_delta.php'><img class="img-responsive" src='img/graph_delta.png'></a></td>
+	<td><a href='infection_level.php'><img class="img-responsive" src='img/infected.png'></a></td>
+	<td><a href='signup.php'><img class="img-responsive" src='img/signup.png'></a></td>
+	<td><a href='https://www.facebook.com/groups/231583938033989/'><img class="img-responsive" src='img/facebook.png'></a></td>
 	<td><div class="fb-share-button" data-href="https://www.covid19math.net<?PHP echo $_SERVER['REQUEST_URI'];?>" data-layout="box_count" data-size="large"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.mdwestserve.com%2Fcoronavirus%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore">Share</a></div></td>
 	
 	</tr></table>
