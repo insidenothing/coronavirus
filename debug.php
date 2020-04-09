46a5102941facbfa4962c28b30796284cad1b7f7
Patrick McGuire <insidenothing@gmail.com>
2020-04-09 11:53:53 -0400

    Update debug.php

--- debug.php
+++ debug.php
@@ -2,16 +2,6 @@
 // break apart the API
 include_once('functions.php');
 
-function getPage($url){
-    $curl = curl_init();
-    curl_setopt ($curl, CURLOPT_URL, $url);
-    curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire Coronavirus Monitor /%d.0",rand(4,50)));
-    curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    $www = curl_exec ($curl);
-    curl_close ($curl);
-    return $www;
-}
 
 $history = make_maryland_array();
 echo '<pre>';
