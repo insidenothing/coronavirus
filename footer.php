4c463d98aac2076b7c1f5e048af57a418a939a8d
Patrick McGuire <insidenothing@gmail.com>
2020-04-10 11:53:49 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,29 @@
+  <div class="row">
+      <div class='col-sm-6'>
+        <img src='img/flag.jpg'>
+      </div>
+    <div class='col-sm-6'>
+        <img src='img/predict.png'>
+      </div>
+  </div>
 
-  <br><img src='img/flag.jpg'><br>
-  &copy 2020 <a target='_Blank' href='https://www.patrickmcguire.me/'>Patrick McGuire Blog</a> 
-| <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a> || <a target='_Blank' href='debug.php'>Developer View</a> | <a target='_Blank' href='https://github.com/insidenothing/coronavirus'>Open Source - View at Github.com</a>
+
+  <div class="row">
+      <div class='col-sm-3'>
+        &copy 2020 <a target='_Blank' href='https://www.patrickmcguire.me/'>Patrick McGuire</a>
+      </div>
+    <div class='col-sm-3'>
+        <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a>
+      </div>
+    <div class='col-sm-3'>
+        <a target='_Blank' href='debug.php'>Developer View</a>
+      </div>
+    <div class='col-sm-3'>
+        <a target='_Blank' href='https://github.com/insidenothing/coronavirus'>Open Source - View at Github.com</a>
+      </div>
+  </div>
+
+
 </div>
 
 <span id="siteseal"><script async type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=JF3ETwtNbyZ345vnJlsp4zh2fEEasPM3vcpVLuhjNIi6TCtnnRaJxL5WiQ5T"></script></span>
