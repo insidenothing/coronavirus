27cb5457823f5345b988b920c122002b2d87e156
Patrick McGuire <insidenothing@gmail.com>
2020-04-10 10:56:15 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
   <br><img src='img/flag.jpg'><br>
-  &copy 2020 <a target='_Blank' href='http://sales.patrickmcguire.me/'>Patrick McGuire</a> 
-| <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a> | <a target='_Blank' href='https://github.com/insidenothing/coronavirus'>Open Source - View at Github.com</a>
+  &copy 2020 <a target='_Blank' href='https://www.patrickmcguire.me/'>Patrick McGuire Blog</a> 
+| <a target='_Blank' href='https://coronavirus.maryland.gov/'>Official Data Source</a> || <a target='_Blank' href='debug.php'>Developer View</a> | <a target='_Blank' href='https://github.com/insidenothing/coronavirus'>Open Source - View at Github.com</a>
 </div>
 
 <span id="siteseal"><script async type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=JF3ETwtNbyZ345vnJlsp4zh2fEEasPM3vcpVLuhjNIi6TCtnnRaJxL5WiQ5T"></script></span>
