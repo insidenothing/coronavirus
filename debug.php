63c3c79f682e51735994c56485ac61aa43f75a05
Patrick McGuire <insidenothing@gmail.com>
2020-04-13 08:33:51 -0400

    Update debug.php

--- debug.php
+++ debug.php
@@ -13,3 +13,12 @@ $history = make_maryland_array2();
 echo '<p>make_maryland_array2()</p><pre>';
 print_r($history);
 echo '</pre>';
+
+
+
+
+$url = 'https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/TEST_ZIPCodeCases/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,ZIPCODE1,ZIPName,ProtectedCount&returnGeometry=false&outSR=4326&f=json';
+$history = make_maryland_array3($url,'');
+echo '<p>make_maryland_array3()</p><pre>';
+print_r($history);
+echo '</pre>';
