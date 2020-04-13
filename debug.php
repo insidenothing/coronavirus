1e2e6d5405a5ca15d06165675898a9a667e51882
Patrick McGuire <insidenothing@gmail.com>
2020-04-13 16:24:01 -0400

    Update debug.php

--- debug.php
+++ debug.php
@@ -2,22 +2,22 @@
 // break apart the API
 include_once('menu.php');
 
-echo "<li>Known URLs</li>";
+echo "<h1>Known URLs</h1>";
 $r = $core->query("select distinct url_pulled from coronavirus");
 while ($d = mysqli_fetch_array($r)){
-  $r2 = $core->query("select * from coronavirus where url_pulled = '$d[url_pulled]' order by id desc");
+  $r2 = $core->query("select * from coronavirus where url_pulled = '$d[url_pulled]' where url_pulled <> '' order by id desc");
   $d2 = mysqli_fetch_array($r2);
-  echo "<li>$d2[checked_datetime] $d[url_pulled]</li>";
+  echo "<p>$d2[checked_datetime] $d[url_pulled]</p>";
 }
 
-echo "<table><tr><td valign='top'>";
+
 
 $history = make_maryland_array();
 echo '<h1>Historic Data</h1><p>make_maryland_array()</p><pre>';
 print_r($history);
 echo '</pre>';
 
-echo "</td><td valign='top'>";
+
 
 $history = make_maryland_array2();
 echo '<h1>Latest Data</h1><p>make_maryland_array2()</p><pre>';
@@ -25,7 +25,7 @@ print_r($history);
 echo '</pre>';
 
 
-echo "</td><td valign='top'>";
+
 
 $url = 'https://services.arcgis.com/njFNhDsUCentVYJW/arcgis/rest/services/TEST_ZIPCodeCases/FeatureServer/0/query?where=1%3D1&outFields=OBJECTID,ZIPCODE1,ZIPName,ProtectedCount&returnGeometry=false&outSR=4326&f=json';
 $history = make_maryland_array3($url,'');
@@ -33,4 +33,4 @@ echo '<h1>Zipcode Data</h1><p>make_maryland_array3()</p><pre>';
 print_r($history);
 echo '</pre>';
 
-echo "</td></tr></table>";
+include_once('footer.php');
