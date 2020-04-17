2a54025fb5b2e82e99579ea5ee99a889144e5d59
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 07:40:12 -0400

    Update sms_test.php

--- sms_test.php
+++ sms_test.php
@@ -1,7 +1,7 @@
 <?PHP
 include_once('menu.php');
 
-// test sending a large image to the phone over multiple messages?
-echo message_send('4433862584','https://www.covid19math.net/img/website-maintenance.png');
+// test sending a link to the phone over multiple messages?
+echo message_send('4433862584','https://www.covid19math.net/msg.php');
 
 include_once('footer.php');
