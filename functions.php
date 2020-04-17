b359de4e036de47e53eb9da146d23874c8530c2d
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 08:04:27 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -39,13 +39,17 @@ function sms($msg){
     	echo '<div>'.$msg.' </div>';	
 }
 
-function message_send($to,$master_message){	
+function message_send($to,$master_message,$media=''){	
+	if ($media != ''){
+		$media="&MediaUrl=$media";
+	}
 	//die('offline');
 	// take full message, split and loop
 	$buffer='';
 	$messages = str_split(strip_tags($master_message), 150);
 	global $twillo_account;
 	global $twillo_key;
+	
 	foreach ($messages as $message) {
 		$message = str_replace('_', ' ', $message);	
 		$curl = curl_init();
@@ -53,7 +57,7 @@ function message_send($to,$master_message){
 		curl_setopt ($curl, CURLOPT_TIMEOUT, '5');
 		curl_setopt ($curl, CURLOPT_RETURNTRANSFER, '1');
 		$message = str_replace(' ','+',$message);
-		$postfields = "From=%2B14433932367&To=$to&Body=$message";
+		$postfields = "From=%2B14433932367&To=$to&Body=$message$media";
 		curl_setopt($curl, CURLOPT_USERPWD, "$twillo_account:$twillo_key"); 
 		curl_setopt ($curl, CURLOPT_POSTFIELDS, $postfields);
 		$bufferX = curl_exec ($curl);
