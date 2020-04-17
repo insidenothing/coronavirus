d388729d6a0ce0bc30fb078762dd6c4c77df3cb8
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 09:36:29 -0400

    Update sms_test.php

--- sms_test.php
+++ sms_test.php
@@ -1,7 +1,7 @@
 <?PHP
 include_once('menu.php');
 
-// test sending a link to the phone over multiple messages? NO SSL!
-echo message_send('4433862584','Testing SMS and MMS','http://covid19math.net/img/website-maintenance.png');
+// NO SSL! php generating png using gd 
+echo message_send('4433862584','https://covid19math.net Update','http://covid19math.net/msg.php');
 
 include_once('footer.php');
