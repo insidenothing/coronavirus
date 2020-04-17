bb6db70f207dd1483fcc38c1f0d0125a47072020
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 10:22:19 -0400

    Update send_sms.php

--- send_sms.php
+++ send_sms.php
@@ -1,23 +1,23 @@
 <?PHP
 include_once('menu.php');
 // make it hard to accidently send - not to be mistaken for security
-if ($_POST['send'] == 'confirm')){
+if ($_POST['send'] == 'confirm'){
 	global $core;
 	$r = $core->query("SELECT sms_number FROM coronavirus_sms where sms_status = 'confirmed' ");
 	while($d = mysqli_fetch_array($r)){
 		$sms = trim($d['sms_number']);
-		message_send($sms,$new_master_message);
-    echo message_send($sms,'Touch the picture to expand http://covid19math.net','http://covid19math.net/msg.php');
+		echo message_send($sms,'Touch the picture to expand http://covid19math.net','http://covid19math.net/msg.php');
 	}
+	$val = 'Sent!';
 }elseif($_POST['send'] != ''){
-  $val = 'Rabbit Hole';
+  	$val = 'Rabbit Hole';
 }else{
-  $val = 'Send';
+  	$val = 'Send SMS';
 }
 ?>
 
 <form method='POST'> 
-<input type='password' name='send'><input type='submit' value='Send'>
+<input type='password' name='send'><input type='submit' value='<?PHP echo $val;?>'>
 </form>
 
 <h3>Current Message</h3>
