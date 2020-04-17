4b96484fd4dd9de0c32cb58f9b738a431158ef4b
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 10:07:01 -0400

    Update msg.php

--- msg.php
+++ msg.php
@@ -1,48 +1,47 @@
 <?PHP $msg="
-up 752 Maryland at 10,784.
-up 43 deaths at 392.
-up 220 total_hospitalized at 2,451.
-up 129 total_released at 736.
-up 2,328 NegativeTests at 48,059.
+up 788 Maryland at 11,572.
+up 33 deaths at 425.
+up 161 total_hospitalized at 2,612.
+up 2,378 NegativeTests at 50,437.
 County by County
-up 3 Allegany at 20.
-up 51 AnneArundel at 896.
-up 31 Baltimore at 1,516.
-up 100 BaltimoreCity at 1,160.
-up 1 Calvert at 103.
-up 1 Caroline at 23.
-up 21 Carroll at 283.
-up 11 Cecil at 101.
-up 17 Charles at 327.
-up 2 Dorchester at 18.
-up 55 Frederick at 497.
-up 9 Harford at 161.
-up 27 Howard at 451.
-up 200 Montgomery at 2,133.
-up 206 PrinceGeorges at 2,722.
-up 1 Somerset at 6.
-down -1 StMarys at 98.
-up 3 Washington at 109.
-up 3 Worcester at 25.
+up 6 Allegany at 26.
+up 70 AnneArundel at 966.
+up 53 Baltimore at 1,569.
+up 113 BaltimoreCity at 1,273.
+up 6 Calvert at 109.
+up 5 Caroline at 28.
+up 5 Carroll at 288.
+up 26 Cecil at 127.
+up 10 Charles at 337.
+up 2 Dorchester at 20.
+up 28 Frederick at 525.
+up 15 Harford at 176.
+up 24 Howard at 475.
+up 3 Kent at 14.
+up 147 Montgomery at 2,280.
+up 244 PrinceGeorges at 2,966.
+up 3 Somerset at 9.
+up 2 StMarys at 100.
+up 7 Washington at 116.
+up 3 Worcester at 28.
 Age Groups
-up 12 case0to9 at 86.
-up 27 case10to19 at 231.
-up 73 case20to29 at 1,162.
-up 114 case30to39 at 1,784.
-up 143 case40to49 at 1,951.
-up 133 case50to59 at 2,138.
-up 128 case60to69 at 1,602.
-up 55 case70to79 at 1,065.
-up 67 case80plus at 765.
+up 11 case10to19 at 242.
+up 65 case20to29 at 1,227.
+up 98 case30to39 at 1,882.
+up 137 case40to49 at 2,088.
+up 149 case50to59 at 2,287.
+up 108 case60to69 at 1,710.
+up 106 case70to79 at 1,171.
+up 114 case80plus at 879.
 Gender
-up 374 Male at 4,967.
-up 378 Female at 5,817.
+up 356 Male at 5,323.
+up 432 Female at 6,249.
 Deltas
-up 192 CaseDelta at 752.
-up 858 NegDelta at 2,328.
-up 111 hospitalizedDelta at 220.
-up 129 releasedDelta at 129.
-down -4 deathsDelta at 43.
+up 36 CaseDelta at 788.
+up 50 NegDelta at 2,378.
+down -59 hospitalizedDelta at 161.
+down -129 releasedDelta at 0.
+down -10 deathsDelta at 33.
 ".date('r');
 
 // GD Code Here
