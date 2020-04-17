f97434c189b265856dcb8cb39361bf05ee98c035
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 09:50:02 -0400

    Update sms_test.php

--- sms_test.php
+++ sms_test.php
@@ -2,6 +2,6 @@
 include_once('menu.php');
 
 // NO SSL! php generating png using gd 
-echo message_send('4433862584','https://covid19math.net Update','http://covid19math.net/msg.php');
+echo message_send('4433862584','Touch the picture to expand http://covid19math.net','http://covid19math.net/msg.php');
 
 include_once('footer.php');
