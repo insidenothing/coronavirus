e0ec9ab40228b0bc06c68b35770d2a4e7f1d7835
Patrick McGuire <insidenothing@gmail.com>
2020-04-17 15:32:45 -0400

    Update zip.php

--- zip.php
+++ zip.php
@@ -20,12 +20,14 @@ function coronavirus_zip($zip,$date,$count,$town){
 global $nocases;
 global $cases;
 global $zipData;
+global $zipData2;
 global $date;
 if(isset($_GET['date'])){
 	$date = $_GET['date'];
 	$r = $core->query("select html from coronavirus where url_pulled like '%ZIPCodes_MD_1%' and just_date = '$date' order by id desc");
 	$d = mysqli_fetch_array($r);
 	$zipData = make_maryland_array3('',$d['html'],'');
+	$zipData2 = make_maryland_array3('',''); // this builds the name array
 }else{
 	$date = date('Y-m-d');
 	$zipData = make_maryland_array3('','');
@@ -38,6 +40,7 @@ function make_datapoints(){
 	global $cases;
 	global $zip2name;
 	global $date;
+	global $zipData2
 	$total = 0;
 	$return = '';
 	foreach ($zipData as $zip => $data){
