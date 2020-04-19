7d20b36b5cd06086b8ebf509b780f2f6b24f3b50
Patrick McGuire <insidenothing@gmail.com>
2020-04-19 10:20:39 -0400

    Update phase1.php

--- phase1.php
+++ phase1.php
@@ -6,7 +6,9 @@ include_once('menu.php');
 <div class="row">
   <div class="col-sm-12">
   <h3>Goals</h3>
-    We are setting up this page to track anything with a downwards trend sorting by days of down trend.
+    We are setting up this page to track zipcodes across the curve. As they trend higher we count the days, 
+    as they flatten off we count the days, as they drop we count the days. Every time the trend changes the
+    duration resets. If you 
   </div>
 </div>
 
@@ -16,31 +18,36 @@ $date = date('Y-m-d');
 ?>
 
 <div class="row">
-  <div class="col-sm-6">
-  <h3>Up Trend</h3>
+  <div class="col-sm-4">
+    <h3>Up Trend</h3>
     <ol>
     <?PHP
     $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_direction = 'UP' order by trend_duration DESC";
+    $r = $core->query($q);
     while($d = mysqli_fetch_array($r)){
         echo "<li>$d[town_name] $d[zip_code] $d[trend_direction] at $d[report_count] for $d[trend_duration] days</li>"; 
     }
     ?>
     </ol>
   </div>
-  <h3>Flat Trend</h3>
-  <ol>
-  <?PHP
+  <div class="col-sm-4">
+    <h3>Flat Trend</h3>
+    <ol>
+    <?PHP
     $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_direction = 'FLAT' order by trend_duration DESC";
+    $r = $core->query($q);
     while($d = mysqli_fetch_array($r)){
         echo "<li>$d[town_name] $d[zip_code] $d[trend_direction] at $d[report_count] for $d[trend_duration] days</li>"; 
     }
     ?>
     </ol>
   </div>
+  <div class="col-sm-4">
   <h3>Down Trend</h3>
-  <ol>
+    <ol>
     <?PHP
     $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_direction = 'DOWN' order by trend_duration DESC";
+    $r = $core->query($q);
     while($d = mysqli_fetch_array($r)){
         echo "<li>$d[town_name] $d[zip_code] $d[trend_direction] at $d[report_count] for $d[trend_duration] days</li>"; 
     }
