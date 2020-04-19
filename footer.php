594b7e90cfc10a876a8ac3ea77e63f1b5b2b9e21
Patrick McGuire <insidenothing@gmail.com>
2020-04-19 09:18:09 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,13 @@
 
 <!--- Footer Menu -->
 <ul class="nav nav-pills" style="text-align:center;">
+		<li role='presentation'><a target='_Blank' href="https://www.facebook.com/groups/covid19md/">Facebook Group</a></li>
 		<li role='presentation'><a target='_Blank' href="https://www.patrickmcguire.me/">&copy 2020 Patricks McGuire</a></li>
 		<li role='presentation' <?PHP if($_SERVER['REQUEST_URI'] == '/debug.php'){ echo "class='active'"; } ?> ><a href="debug.php">Raw Data</a></li>
 		<li role='presentation'><a target='_Blank' href='https://github.com/insidenothing/coronavirus/blob/master<?PHP echo $_SERVER['SCRIPT_NAME'];?>'>Open Source - Edit <?PHP echo $_SERVER['SCRIPT_NAME'];?> at Github.com</a></li>
 		<li role='presentation'><a target='_Blank' href="https://coronavirus.maryland.gov/">Official Data</a></li>
-		<li role='presentation'><span id="siteseal"><script async type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=JF3ETwtNbyZ345vnJlsp4zh2fEEasPM3vcpVLuhjNIi6TCtnnRaJxL5WiQ5T"></script></span></li>
 		<li role='presentation'><a target='_Blank' href="http://www.ciclt.net/sn/clt/capitolimpact/gw_countymap.aspx?State=md&StFIPS=&StName=Maryland">ZIP Data</a></li>
+		<li role='presentation'><span id="siteseal"><script async type="text/javascript" src="https://seal.starfieldtech.com/getSeal?sealID=JF3ETwtNbyZ345vnJlsp4zh2fEEasPM3vcpVLuhjNIi6TCtnnRaJxL5WiQ5T"></script></span></li>
 </ul>
 
 
