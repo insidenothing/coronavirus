6962d789939fb894ce8f1ca54fccd53c882a5d58
Patrick McGuire <insidenothing@gmail.com>
2020-04-19 09:13:21 -0400

    Create phase1.php

---
+++ phase1.php
@@ -0,0 +1,15 @@
+<?PHP
+$page_description = "Maryland Reopen - Phase One  - Status";
+include_once('menu.php');
+?>
+<div class="row">
+  <div class="col-sm-12">
+  <h3>Goals</h3>
+    We are setting up this page to track anything with a downwards trend sorting by days of down trend.
+  </div>
+</div>
+
+
+
+
+<?PHP include_once('footer.php'); ?>
