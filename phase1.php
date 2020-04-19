350d67279be203b86ff9f4b8ba7ba772e3743e0e
Patrick McGuire <insidenothing@gmail.com>
2020-04-19 10:16:37 -0400

    Update phase1.php

--- phase1.php
+++ phase1.php
@@ -10,6 +10,44 @@ include_once('menu.php');
   </div>
 </div>
 
+<?PHP
+// pull date from last update, not assume today.
+$date = date('Y-m-d');
+?>
+
+<div class="row">
+  <div class="col-sm-6">
+  <h3>Up Trend</h3>
+    <ol>
+    <?PHP
+    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_direction = 'UP' order by trend_duration DESC";
+    while($d = mysqli_fetch_array($r)){
+        echo "<li>$d[town_name] $d[zip_code] $d[trend_direction] at $d[report_count] for $d[trend_duration] days</li>"; 
+    }
+    ?>
+    </ol>
+  </div>
+  <h3>Flat Trend</h3>
+  <ol>
+  <?PHP
+    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_direction = 'FLAT' order by trend_duration DESC";
+    while($d = mysqli_fetch_array($r)){
+        echo "<li>$d[town_name] $d[zip_code] $d[trend_direction] at $d[report_count] for $d[trend_duration] days</li>"; 
+    }
+    ?>
+    </ol>
+  </div>
+  <h3>Down Trend</h3>
+  <ol>
+    <?PHP
+    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_direction = 'DOWN' order by trend_duration DESC";
+    while($d = mysqli_fetch_array($r)){
+        echo "<li>$d[town_name] $d[zip_code] $d[trend_direction] at $d[report_count] for $d[trend_duration] days</li>"; 
+    }
+    ?>
+    </ol>
+  </div>
+</div>
 
 
 
