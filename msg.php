ba13e095f21f6cf0269aed4d9a93185b170696b9
Patrick McGuire <insidenothing@gmail.com>
2020-04-20 10:22:56 -0400

    Update msg.php

--- msg.php
+++ msg.php
@@ -1,117 +1,17 @@
 <?PHP 
 include_once('/var/www/secure.php'); //outside webserver
 include_once('functions.php'); //outside webserver
-global $attributes;
-global $maryland_history;
-global $new_id;
-global $new_date;
-global $old_date;
-global $core;
-$maryland_history = make_maryland_array();
 
-function attribute_aka($county){
-	global $attributes;
-	if ($county == 'caseAfrAmer'){ return $attributes['raceAfrAmer']['CaseCount']; }
-	if ($county == 'deathAfrAmer'){ return $attributes['raceAfrAmer']['DeathCount']; }
-	
-	if ($county == 'caseWhite'){ return $attributes['raceWhite']['CaseCount']; }
-	if ($county == 'deathWhite'){ return $attributes['raceWhite']['DeathCount']; }
-	
-	if ($county == 'caseAsian'){ return $attributes['raceAsian']['CaseCount']; }
-	if ($county == 'deathAsian'){ return $attributes['raceAsian']['DeathCount']; }
-	
-	if ($county == 'caseOther'){ return $attributes['raceOther']['CaseCount']; }
-	if ($county == 'deathOther'){ return $attributes['raceOther']['DeathCount']; }
-	
-	if ($county == 'caseNotAVail'){ return $attributes['raceNotAvail']['CaseCount']; }
-	if ($county == 'deathNotAvail'){ return $attributes['raceNotAvail']['DeathCount']; }
-}
+$q = "select * from coronavirus_msg order by id desc limit 1";
+$r = $core->query("SELECT id, checked_datetime FROM coronavirus order by id DESC limit 0,1");
+$d = mysqli_fetch_array($r);
+$date = $d['msg_made_datetime'];
+$clean = strip_tags($d['msg']);
 
-function do_math_location($county){
-	
-	global $maryland_history;
-	global $new_id;
-	global $new_date;
-	global $old_date;
-	global $core;
-	$today = date('Y-m-d',strtotime($new_date));
-	$aka = county_aka($county);
-	$count_today = $maryland_history[$today][$aka];
-	
-	$yesterday = date('Y-m-d',strtotime($old_date));
-	$count_yesterday = $maryland_history[$yesterday][$aka];
-	$core->query("update coronavirus set $countyCOVID19Cases = '$count_today' where id = '$new_id' ");
-	
-	if ($count_today == 0){
-		// failure detected
-		// check attributes
-		$count_fix = attribute_aka($county);
-		//echo "<p>PATCH $count_today to $count_fix for $county ($count_yesterday)</p>";
-		$count_today = $count_fix;
-	}
-	
-	$count_delta = $count_today - $count_yesterday;
-	$dir = 'up';
-	if ( $count_today < $count_yesterday){
-		$dir = 'down';
-	}
-	$human_count = number_format($count_today);
-	$human_delta = number_format($count_delta);
-	if ($count_delta != 0) { sms("$dir $human_delta <b>$county</b> at $human_count. ");  } 
-}
-
-ob_start();
-// V3
-echo do_math_location('Maryland');
-echo do_math_location('deaths');
-echo do_math_location('total_hospitalized');
-echo do_math_location('total_released');
-echo do_math_location('NegativeTests');
-echo '<h4>County by County</h4>';
-echo do_math_location('Allegany');
-echo do_math_location('AnneArundel');
-echo do_math_location('Baltimore');
-echo do_math_location('BaltimoreCity');
-echo do_math_location('Calvert');
-echo do_math_location('Caroline');
-echo do_math_location('Carroll');
-echo do_math_location('Cecil');
-echo do_math_location('Charles');
-echo do_math_location('Dorchester');
-echo do_math_location('Frederick');
-echo do_math_location('Garrett');
-echo do_math_location('Harford');
-echo do_math_location('Howard');
-echo do_math_location('Kent');
-echo do_math_location('Montgomery');
-echo do_math_location('PrinceGeorges');
-echo do_math_location('QueenAnnes');
-echo do_math_location('Somerset');
-echo do_math_location('StMarys');
-echo do_math_location('Talbot');
-echo do_math_location('Washington');
-echo do_math_location('Worcester');
-echo '<h4>Age Groups</h4>';
-echo do_math_location('case0to9');
-echo do_math_location('case10to19');
-echo do_math_location('case20to29');
-echo do_math_location('case30to39');
-echo do_math_location('case40to49');
-echo do_math_location('case50to59');
-echo do_math_location('case60to69');
-echo do_math_location('case70to79');
-echo do_math_location('case80plus');
-echo '<h4>Deltas</h4>';	
-echo do_math_location('CaseDelta');
-echo do_math_location('NegDelta');
-echo do_math_location('hospitalizedDelta');
-echo do_math_location('releasedDelta');
-echo do_math_location('deathsDelta');	
-$new_master_message = ob_get_clean();
-$clean = strip_tags($new_master_message);
 $msg="
 $clean
-".date('r');
+".$date;
+
 
 // GD Code Here
 header('Content-Type: image/png');
