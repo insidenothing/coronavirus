49307d366058d82d03ad5ab79af491a40c309f01
Patrick McGuire <insidenothing@gmail.com>
2020-04-21 11:22:00 -0400

    Update death_level.php

--- death_level.php
+++ death_level.php
@@ -33,7 +33,8 @@ function infection_levelMD($county){
 	$a = $cases / $gcd;
 	$b = $total / $gcd;
 	$c =  $cases / $total;
-	$d = $c * 1000;
+	$d = $c;
+	//$d = $c * 1000;
 	$state_level = $d;
 	$e = number_format($d, 5, '.', '');
 	$core->query("update coronavirus_populations set rate_of_death = '$e' where name_of_location = '$county' ");
@@ -48,7 +49,8 @@ function infection_level($county){
 	$a = $cases / $gcd;
 	$b = $total / $gcd;
 	$c =  $cases / $total;
-	$d = $c * 1000;
+	$d = $c;
+	//$d = $c * 1000;
 	$e = number_format($d, 5, '.', '');
 	if($cases == 0){
 		echo "<div class='col-sm-2' style='background-color:lightgreen;'><h4>COLD <a href='county.php?county=$county'>$county</a></h4><p>$cases : $total</p><p>$a : $b</p><p><b>$e%</b></p></div>";	
