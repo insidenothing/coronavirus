cecad971759790ec216a593352b2b5a0222cfd95
Patrick McGuire <insidenothing@gmail.com>
2020-04-22 09:19:58 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -6,10 +6,10 @@ function make_florida_zip_array($url='',$json='',$force=''){
 	global $arcgis_key;
 	global $core;
 	$return = array();
-	if ($json != ''){
+	/*if ($json != ''){
 		$array = json_decode($json, true);
 		return $array;
-	}
+	}*/
 	$url = 'https://services1.arcgis.com/CY1LXxl9zlJeBuRZ/ArcGIS/rest/services/COVID_19_Cases_in_Florida_by_Zip_Code/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=*&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=';
 	$return['url_pulled'] = $url;
 	if($force == ''){
