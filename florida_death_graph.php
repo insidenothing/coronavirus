1bae688c0b322717a85e0047a302d303e96f0f8e
Patrick McGuire <insidenothing@gmail.com>
2020-04-30 17:22:54 -0400

    Update florida_death_graph.php

--- florida_death_graph.php
+++ florida_death_graph.php
@@ -1,103 +1,23 @@
 <?PHP
-global $show;
-$show = '';
-if(isset($_GET['show'])){
-	$show = $_GET['show'];
-}
-global $days_to_predict;
-$days_to_predict = '1';
-
-if(isset($_POST['days'])){
-	$days_to_predict = $_POST['days'];	
-}
-if(isset($_GET['day'])){
-	$days_to_predict = $_GET['day'];	
-}
-$logo = 'off';
 $page_description = "Florida Death Data";
 include_once('menu.php');
-
-global $today;
-global $normal;
-global $peak;
-global $peak_str;
-$today = array();
-$normal = array();
-$peak = array();
-$peak_str = array();
-
 global $florida_deaths;
 $florida_deaths = $florida = make_florida_zip_array2('','','');
-
-
-
-
-global $buffer;
-$buffer = '.85';
-if(isset($_POST['buffer'])){
-	$buffer = $_POST['buffer'];	
-}
-global $debug_in;
-global $debug_out;
-
-function total_count($county){
-	global $core;
-	$q = "SELECT number_of_people FROM coronavirus_populations where name_of_location = '$county' ";
-	$r = $core->query($q);
-	$d = mysqli_fetch_array($r);
-	return $d['number_of_people'];
-}
-function rate_of_infection($county){
-	global $core;
-	$q = "SELECT rate_of_infection FROM coronavirus_populations where name_of_location = '$county' ";
-	$r = $core->query($q);
-	$d = mysqli_fetch_array($r);
-	return $d['rate_of_infection'];	
-}
-function show_on_graph($county){
-	global $show;
-	// if has cases = true else false
-	global $maryland_history;
-	$date = $maryland_history['date'];
-	$aka = county_aka($county);
-	error_log("show_on_graph($county) $date $aka", 0);
-	$val = $maryland_history[$date][$aka];
-	$count = intval($val);
-	if ($show != ''){
-		if ($show == $county){
-			return 'true';	
-		}else{
-			return 'false';	
-		}	
-	}
-	if ($count == 0){
-		return 'false';
-	}else{
-		return 'true';	
-	}
-	
-}
 function make_fl_deaths(){
         global $core;
         global $florida_deaths;
         $return = '';
-        $t = '0'; // days
-        $dt= '1'; // change in days
-
-
 	$count=0;
 	global $today;
 	foreach ($florida_deaths as $date => $array){
 		$last_count = $count;
 		$count = intval($array['Deaths']);
-		$return .= '{ label: "'.$date.'", y: '.$count.' }, ';
+		$return .= '{ label: "'.date('Y-m-d',$date).'", y: '.$count.' }, ';
 		$today[$county] = $count;
 	}
-  
-    	$return = rtrim(trim($return), ",");
+      	$return = rtrim(trim($return), ",");
     return $return;
 }
-
 ?>
 <script>
 window.onload = function () {
@@ -131,10 +51,8 @@ var chart = new CanvasJS.Chart("chartContainer", {
 			<?PHP echo make_fl_deaths(); ?>
 		]
 	}]
-}
-			      
-			      
-			      );
+}			      			      
+);
 chart.render();
 
 function toggleDataSeries(e) {
@@ -151,9 +69,5 @@ function toggleDataSeries(e) {
 
 <div id="chartContainer" style="height: 370px; max-width: 1020px; margin: 0px auto;"></div>
 <script src="canvasjs.min.js"></script>
-
-
-
-
 
 <?PHP include_once('footer.php'); ?>
