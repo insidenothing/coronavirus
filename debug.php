60f53c6b04db62731f02ac9bd2be538323b6c7b9
Patrick McGuire <insidenothing@gmail.com>
2020-05-27 10:47:26 -0400

    Update debug.php

--- debug.php
+++ debug.php
@@ -2,47 +2,14 @@
 // break apart the API
 include_once('menu.php');
 
+  $date = date('Y-m-d');
+	$r = $core->query("select raw_response from coronavirus_api_cache where id = '13' and just_date = '$date' order by id desc");
+	$d = mysqli_fetch_array($r);
+	$zipData = make_maryland_array3('',$d['raw_response'],'');
+	$zipData2 = make_maryland_array3('',''); // this builds the name array
 
-echo "<div style='text-align:left;'><h1>Known URLs</h1>";
-$r = $core->query("select distinct url_pulled from coronavirus where url_pulled <> '' order by id desc");
-while ($d = mysqli_fetch_array($r)){
-  $r2 = $core->query("select * from coronavirus where url_pulled = '$d[url_pulled]' order by id desc");
-  $d2 = mysqli_fetch_array($r2);
-  $row_cnt = mysqli_num_rows($r2);
-  //if(substr($d2['checked_datetime'],0,10) == date('Y-m-d')){
-  echo "<p><b>$d2[checked_datetime] ($row_cnt updates)</b> <small>$d[url_pulled]</small></p>
-  <div style='border:ridge 5px blue; font-size:8px;'><pre>$d2[raw_response]</pre></div>";
-  //}
-}
+echo "<pre>".print_r($zipData)."</pre>";
 
-
-/*
-$history = make_maryland_array();
-echo '<h1>Historic Data</h1><p>make_maryland_array()</p><pre>';
-print_r($history);
-echo '</pre>';
-
-
-
-$history = make_maryland_array2();
-echo '<h1>Latest Data</h1><p>make_maryland_array2()</p><pre>';
-print_r($history);
-echo '</pre>';
-
-
-
-
-$history = make_maryland_array3('','');
-echo '<h1>Zipcode Data</h1><p>make_maryland_array3()</p><pre>';
-print_r($history);
-echo '</pre></div>';
-
-
-echo '<h1>DEBUG</h1>';
-global $debug;
-echo $debug;
-echo '</div>';
-*/
-
+echo "<pre>".print_r($zipData2)."</pre>";
 
 include_once('footer.php');
