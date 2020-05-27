d8eee75438cb8ca3ea9f47582374f9aaacae16ce
Patrick McGuire <insidenothing@gmail.com>
2020-05-27 16:03:13 -0400

    Update debug.php

--- debug.php
+++ debug.php
@@ -8,7 +8,13 @@ if(isset($_GET['id'])){
 }
 $r = $core->query("select raw_response from coronavirus_api_cache where api_id = '$id' order by id desc");
 $d = mysqli_fetch_array($r);
-$zipData = make_maryland_array3($d['raw_response']);
+if (isset($_GET['date'])){
+ $date_formatted = $_GET['date'];
+}else{
+ $date_formatted = 'total'.date('m_d_Y');
+}
+
+$zipData = make_maryland_array3($d['raw_response'],$date_foramted);
 //$zipData = format_json($d['raw_response']);
 echo "<h1>INPUT</h1>";
 echo "<div>".$d['raw_response']."</div>";
