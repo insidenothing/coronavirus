707fbe2841041a86ba2e421c53ba936a9e2ea401
Patrick McGuire <insidenothing@gmail.com>
2020-05-28 11:41:31 -0400

    Update zipcode.php

--- zipcode.php
+++ zipcode.php
@@ -97,7 +97,7 @@ window.onload = function () {
 		animationEnabled: true,
 		exportEnabled: true,
 		title:{
-			text: "<?PHP echo $name;?> Cases over Time covid19math.net"
+			text: "<?PHP echo $name;?> Cases - 14 days covid19math.net"
 		},
 		axisY :{
 			includeZero: false,
