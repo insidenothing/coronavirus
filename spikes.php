ac9898aee7523f27a06f9e6bacb46aef2b64a945
Patrick McGuire <insidenothing@gmail.com>
2020-05-28 23:33:29 -0400

    Update spikes.php

--- spikes.php
+++ spikes.php
@@ -17,7 +17,7 @@ $date = date('Y-m-d');
 	<div class="col-sm-3">
     <h1>14 Day</h1><ol>
     <?PHP
-    	$q = "SELECT day14change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day14change_percentage <> '' order by day7change_percentage DESC";
+    	$q = "SELECT day14change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day14change_percentage <> '' order by day14change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
 		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day14change_percentage']."%</a></li>";
@@ -27,7 +27,7 @@ $date = date('Y-m-d');
   <div class="col-sm-3">
     <h1>30 Day</h1><ol>
     <?PHP
-    	$q = "SELECT day30change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day30change_percentage <> '' order by day7change_percentage DESC";
+    	$q = "SELECT day30change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day30change_percentage <> '' order by day30change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
 		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day30change_percentage']."%</a></li>";
@@ -37,7 +37,7 @@ $date = date('Y-m-d');
 	<div class="col-sm-3">
    <h1>45 Day</h1><ol>
    <?PHP
-    	$q = "SELECT day45change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day45change_percentage <> '' order by day7change_percentage DESC";
+    	$q = "SELECT day45change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day45change_percentage <> '' order by day45change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
 		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day45change_percentage']."%</a></li>";
