4e786c501dee2f12a42bc324d6db4b407be00047
Patrick McGuire <insidenothing@gmail.com>
2020-05-28 23:22:08 -0400

    Create spikes.php

---
+++ spikes.php
@@ -0,0 +1,23 @@
+<?PHP
+$page_description = "Maryland COVID 19 Spike Monitor";
+include_once('menu.php');
+?>
+<div class="row">
+	<div class="col-sm-3">
+    <h1>7 Day</h1>
+    
+  </div>
+	<div class="col-sm-3">
+    <h1>14 Day</h1>
+    
+  </div>
+  <div class="col-sm-3">
+    <h1>30 Day</h1>
+    
+  </div>
+	<div class="col-sm-3">
+   <h1>45 Day</h1>
+   
+  </div>
+</div>
+<?PHP include_once('footer.php'); ?>
