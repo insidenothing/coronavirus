f0a417123d79f6b6a63e1c4fad94aa4992148b42
Patrick McGuire <insidenothing@gmail.com>
2020-05-28 23:32:22 -0400

    Update spikes.php

--- spikes.php
+++ spikes.php
@@ -5,44 +5,44 @@ $date = date('Y-m-d');
 ?>
 <div class="row">
 	<div class="col-sm-3">
-    	<h1>7 Day</h1>
+    	<h1>7 Day</h1><ol>
 	<?PHP
     	$q = "SELECT day7change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day7change_percentage <> '' order by day7change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
-		echo "<li>".$d['zip_code']." ".$d['day7change_percentage']."%</li>";
+		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day7change_percentage']."%</a></li>";
 	}
-	?>
+		?></ol>
   </div>
 	<div class="col-sm-3">
-    <h1>14 Day</h1>
+    <h1>14 Day</h1><ol>
     <?PHP
     	$q = "SELECT day14change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day14change_percentage <> '' order by day7change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
-		echo "<li>".$d['zip_code']." ".$d['day14change_percentage']."%</li>";
+		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day14change_percentage']."%</a></li>";
 	}
-	?>
+		?></ol>
   </div>
   <div class="col-sm-3">
-    <h1>30 Day</h1>
+    <h1>30 Day</h1><ol>
     <?PHP
     	$q = "SELECT day30change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day30change_percentage <> '' order by day7change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
-		echo "<li>".$d['zip_code']." ".$d['day30change_percentage']."%</li>";
+		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day30change_percentage']."%</a></li>";
 	}
-	?>
+	  ?></ol>
   </div>
 	<div class="col-sm-3">
-   <h1>45 Day</h1>
+   <h1>45 Day</h1><ol>
    <?PHP
     	$q = "SELECT day45change_percentage, zip_code FROM coronavirus_zip where report_date = '$date' and day45change_percentage <> '' order by day7change_percentage DESC";
 	$r = $core->query($q);
 	while ($d = mysqli_fetch_array($r)){
-		echo "<li>".$d['zip_code']." ".$d['day45change_percentage']."%</li>";
+		echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." ".$d['day45change_percentage']."%</a></li>";
 	}
-	?>
+		?></ol>
   </div>
 </div>
 <?PHP include_once('footer.php'); ?>
