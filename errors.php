ee397d9929a4d5dae76cf10b32746a32cbfc2829
Patrick McGuire <insidenothing@gmail.com>
2020-05-31 15:25:59 -0400

    Update errors.php

--- errors.php
+++ errors.php
@@ -3,6 +3,7 @@ include_once('blocked_bots.php');
 include_once('/var/www/secure.php');
 ?>
 <h1>Nothing works perfectly<h1>
+  
 <h2>Monitor 7 Day</h2>
 <ol>
 <?PHP
@@ -20,26 +21,8 @@ while($d = mysqli_fetch_array($r)){
 ?>
 </ol>
 
-  
-  <h2>Monitor 7 Day</h2>
-<ol>
-<?PHP
-$q = "SELECT * FROM coronavirus_zip where day7change_percentage < 0 order by day7change_percentage";
-$r = $core->query($q);
-while($d = mysqli_fetch_array($r)){	
-  $time = strtotime($d['report_date']) - 86400;
-  $r_date = date('Y-m-d',$time);
-  $q2 = "SELECT day7change_percentage FROM coronavirus_zip where zip_code = '$d[zip_code]' and report_date = '$r_date' ";
-  $r2 = $core->query($q2);
-  $d2 = mysqli_fetch_array($r2);
-  echo "<li>Updating record $d[id] zip $d[zip_code] at $d[day7change_percentage] on $d[report_date] for math error with $d2[day7change_percentage].</li>";
-  $core->query("update coronavirus_zip set day7change_percentage = '$d2[day7change_percentage]' where id = '$d[id]' ");
-}
-?>
-</ol>
-
-  
-  <h2>Monitor 14 Day</h2>
+    
+<h2>Monitor 14 Day</h2>
 <ol>
 <?PHP
 $q = "SELECT * FROM coronavirus_zip where day14change_percentage < 0 order by day14change_percentage";
@@ -58,7 +41,7 @@ while($d = mysqli_fetch_array($r)){
 
   
   
-  <h2>Monitor 30 Day</h2>
+<h2>Monitor 30 Day</h2>
 <ol>
 <?PHP
 $q = "SELECT * FROM coronavirus_zip where day30change_percentage < 0 order by day30change_percentage";
@@ -77,7 +60,7 @@ while($d = mysqli_fetch_array($r)){
 
   
   
-  <h2>Monitor 45 Day</h2>
+<h2>Monitor 45 Day</h2>
 <ol>
 <?PHP
 $q = "SELECT * FROM coronavirus_zip where day45change_percentage < 0 order by day45change_percentage";
