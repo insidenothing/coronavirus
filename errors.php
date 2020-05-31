30c36c50b7f0b4d9c49d654581cadc433ec78967
Patrick McGuire <insidenothing@gmail.com>
2020-05-31 14:46:08 -0400

    Update errors.php

--- errors.php
+++ errors.php
@@ -6,10 +6,10 @@ include_once('/var/www/secure.php');
 <h2>try to monitor for issues here</h2>
 <ol>
 <?PHP
-$q = "SELECT distinct id, zip_code, day7change_percentage FROM `coronavirus_zip` where day7change_percentage < 0 order by day7change_percentage";
+$q = "SELECT id, zip_code, day7change_percentage, report_date FROM `coronavirus_zip` where day7change_percentage < 0 order by day7change_percentage";
 $r = $core->query($q);
 while($d = mysqli_fetch_array($r)){	
-  echo "<li>Check $d[id] $d[zip_code] $d[day7change_percentage] for math error.</li>";
+  echo "<li>Check record $d[id] zip $d[zip_code] at $d[day7change_percentage] on $d[report_date] for math error.</li>";
 }
 ?>
 </ol>
