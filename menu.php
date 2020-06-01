f2b4c77eb1a9f4b52bd8e39588f370bcd83c702e
Patrick McGuire <insidenothing@gmail.com>
2020-06-01 09:51:00 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -140,12 +140,13 @@ while($d = mysqli_fetch_array($r)){
 		
 		</ul>
 	</form>
-	<?PHP 
-	/*
+	
+	
 	<button type="button" class="btn btn-success" data-toggle="collapse" data-target="#demo">Maryland Counties</button><button type="button" class="btn btn-info" data-toggle="collapse" data-target="#demo2">Florida Counties</button>
 	<ul id="demo" class="nav nav-tabs collapse">
 		<?PHP echo "$links"; ?>
 	</ul>
+	<?PHP 	/*
 	<ul id="demo2" class="nav nav-tabs collapse">
 		<li role='presentation' <?PHP if($_SERVER['REQUEST_URI'] == '/county.php?state=Florida&county=PalmBeach'){ echo "class='active'"; } ?> ><a href="county.php?state=Florida&county=PalmBeach">PalmBeach</a></li>
 	</ul>
