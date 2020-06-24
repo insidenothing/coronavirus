66197365f89fb29abee0d90316632459e977b340
Patrick McGuire <insidenothing@gmail.com>
2020-06-24 15:52:58 -0400

    Update outbreak.php

--- outbreak.php
+++ outbreak.php
@@ -11,8 +11,8 @@ while($d = mysqli_fetch_array($r)){
 	$zipcode[$zip] = $d['town_name'];
 }
 $date = $global_date;
-echo "<h1>Possible Maryland Outbreak Monitor for $date</h1>";
-echo "<h3>This list of ZIP codes are seeing a percentage increase in cases at 7, 14, 30, and 45 days.</h3><table width='100%' border='1' cellpadding='10' cellspacing='0'>";
+echo "<h1>Maryland Outbreak Monitor for $date</h1>";
+echo "<h3>This table of ZIP codes are seeing a percentage increase in cases at 7, 14, 30, and 45 days.</h3><table width='100%' border='1' cellpadding='10' cellspacing='0'>";
 $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and percentage_direction = 'up' and percentage_direction14 = 'up' and percentage_direction30 = 'up' and percentage_direction45 = 'up' order by report_count DESC";
 $r = $core->query($q);
 while ($d = mysqli_fetch_array($r)){
@@ -23,4 +23,14 @@ while ($d = mysqli_fetch_array($r)){
   ".$d['day30change_percentage']."%</td><td>45 Days ".$d['day45change_percentage']."%</td><td>".$d['report_count']." infections</tr>";
 }
 echo "</table>";
+echo "<h1>Maryland Outbreak Monitor for $date</h1>";
+echo "<h3>This list of ZIP codes are seeing a percentage increase in cases at 7, 14, 30, and 45 days.</h3><ol>";
+$q = "SELECT * FROM coronavirus_zip where report_date = '$date' and percentage_direction = 'up' and percentage_direction14 = 'up' and percentage_direction30 = 'up' and percentage_direction45 = 'up' order by report_count DESC";
+$r = $core->query($q);
+while ($d = mysqli_fetch_array($r)){
+  $zip_c = $d['zip_code'];
+  $name = $zipcode[$zip_c];
+  echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['report_count']." infections</li>";
+}
+echo "</ol>";
 include_once('footer.php');
