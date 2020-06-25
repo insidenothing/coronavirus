328458c4631ec9d9dc30c5325ffaa35df1c3734d
Patrick McGuire <insidenothing@gmail.com>
2020-06-25 17:46:16 -0400

    Update zipcode.php

--- zipcode.php
+++ zipcode.php
@@ -186,7 +186,7 @@ $day7 = make_chart('7');
 $day14 = make_chart('14');
 $day30 = make_chart('30');
 $day45 = make_chart('45');
-$day90 = make_chart('90');
+$day90 = make_chart('60');
 $page_description = $day14['page_description'];
 include_once('menu.php');
 $date = $global_date;
