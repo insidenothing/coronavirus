ac441f2ce5d5a2e319ab859c508d3c61b50ea7f7
Patrick McGuire <insidenothing@gmail.com>
2020-06-26 12:34:09 -0400

    Update active_covid.php

--- active_covid.php
+++ active_covid.php
@@ -15,12 +15,12 @@ $date = $global_date;
 
 echo "<h1>Maryland Active COVID-19 Cases for $date</h1>";
 echo "<h3>This list of ZIP codes have active cases. Cases are removed after 14 days.</h3><ol>";
-$q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date'  order by active_cases DESC";
+$q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date'  order by active_count DESC";
 $r = $core->query($q);
 while ($d = mysqli_fetch_array($r)){
   $zip_c = $d['zip_code'];
   $name = $zipcode[$zip_c];
-  echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_cases']." infections</li>";
+  echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_count']." infections</li>";
 }
 echo "</ol>";
 
