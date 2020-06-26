65373e92ebc0a8b6d52605a4eeee96d45c4e548d
Patrick McGuire <insidenothing@gmail.com>
2020-06-26 16:56:39 -0400

    Update facilities.php

--- facilities.php
+++ facilities.php
@@ -1,5 +1,5 @@
 <?PHP
-// pull assisted living, etc. numbers and store along side zip_codes 
+// pull assisted living numbers and store just like side zip_codes 
 
 // $array[name] = zip
 $Facility_Name['Anchorage_Healthcare_Center'] = '21801';
@@ -82,46 +82,105 @@ $Facility_Name['ManorCare Health Services- Rossville'] = '21237';
 $Facility_Name['ManorCare Health Services- Towson'] = '21286';
 $Facility_Name['Maria Health Care Center'] = '21212';
 $Facility_Name['Meadow Park Rehabilitation and Healthcare'] = '21228';
-$Facility_Name['Montgomery Village Health Care Center'] = '';
-$Facility_Name['Morningside House of Ellicott City'] = '';
-$Facility_Name['North Oak'] = '';
-$Facility_Name['Northwest Health and Rehab Center'] = '';
-$Facility_Name['Nursing and Rehab Center at Stadium Place'] = '';
-$Facility_Name['Oak Crest'] = '';
-$Facility_Name['Orchard Hill Health and Rehab'] = '';
-$Facility_Name['Patapsco Valley Center'] = '';
-$Facility_Name['Peak Health Caton Manor'] = '';
-$Facility_Name['Peake Healthcare at Hartley Hall (Autumn Lake Healthcare at Hartley Hall)'] = '';
-$Facility_Name['Peake Healthcare at the Pines'] = '';
-$Facility_Name['Pleasant View Nursing Home'] = '';
-$Facility_Name['Post Acute Care Center'] = '';
-$Facility_Name['Potomac Valley Rehabilitation and Healthcare Center'] = '';
-$Facility_Name['PowerBack Rehabilitation, Brightwood Campus'] = '';
-$Facility_Name['Regency Care of Silver Spring'] = '';
-$Facility_Name['SagePoint Nursing and Rehabilitation'] = '';
-$Facility_Name['Salisbury Rehabilitation and Nursing Center'] = '';
-$Facility_Name['Seabury at Springvale Terrace'] = '';
-$Facility_Name['Shady Grove Center for Nursing & Rehabilitation'] = '';
-$Facility_Name['Shangri-La Assisted Living'] = '';
-$Facility_Name['Sligo Creek Center - Peak Healthcare'] = '';
-$Facility_Name['Spa Creek Center'] = '';
-$Facility_Name['St. Mary's Nursing Center'] = '';
-$Facility_Name['Stella Maris'] = '';
-$Facility_Name['Sterling Care South Mountain'] = '';
-$Facility_Name['Summit Park Health & Rehabilitation Center'] = '';
-$Facility_Name['Sunrise of Columbia'] = '';
-$Facility_Name['Sunrise Senior Living Bethesda'] = '';
-$Facility_Name['The Charleston Senior Community'] = '';
-$Facility_Name['The Charleston Senior Community'] = '';
-$Facility_Name['The Neighborhoods at St. Elizabeth Rehabilitation & Nursing Center'] = '';
-$Facility_Name['The Peartree House Assisted Living'] = '';
-$Facility_Name['The Resort at Chester Manor'] = '';
-$Facility_Name['Tribute at Black Hill'] = '';
-$Facility_Name['Tudor Heights Assisted Living'] = '';
-$Facility_Name['Village at Augsburg -- Nursing Home'] = '';
-$Facility_Name['Village at Rockville'] = '';
-$Facility_Name['Westgate Hills Rehabilitation & Healthcare Center'] = '';
-$Facility_Name['Westminster Healthcare Center'] = '';
+$Facility_Name['Montgomery Village Health Care Center'] = '20886';
+$Facility_Name['Morningside House of Ellicott City'] = '21042';
+$Facility_Name['North Oak'] = '21208';
+$Facility_Name['Northwest Health and Rehab Center'] = '21215';
+$Facility_Name['Nursing and Rehab Center at Stadium Place'] = '21218';
+$Facility_Name['Oak Crest'] = '21234';
+$Facility_Name['Orchard Hill Health and Rehab'] = '21204';
+$Facility_Name['Patapsco Valley Center'] = '21133';
+$Facility_Name['Peak Health Caton Manor'] = '21229';
+$Facility_Name['Peake Healthcare at Hartley Hall (Autumn Lake Healthcare at Hartley Hall)'] = '21851';
+$Facility_Name['Peake Healthcare at the Pines'] = '21601';
+$Facility_Name['Pleasant View Nursing Home'] = '21771';
+$Facility_Name['Post Acute Care Center'] = '21206';
+$Facility_Name['Potomac Valley Rehabilitation and Healthcare Center'] = '20850';
+$Facility_Name['PowerBack Rehabilitation, Brightwood Campus'] = '21093';
+$Facility_Name['Regency Care of Silver Spring'] = '20910';
+$Facility_Name['SagePoint Nursing and Rehabilitation'] = '20646';
+$Facility_Name['Salisbury Rehabilitation and Nursing Center'] = '21804';
+$Facility_Name['Seabury at Springvale Terrace'] = '20910';
+$Facility_Name['Shady Grove Center for Nursing & Rehabilitation'] = '20850';
+$Facility_Name['Shangri-La Assisted Living'] = '21043';
+$Facility_Name['Sligo Creek Center - Peak Healthcare'] = '20912';
+$Facility_Name['Spa Creek Center'] = '21403';
+$Facility_Name['St. Marys Nursing Center'] = '20650';
+$Facility_Name['Stella Maris'] = '21093';
+$Facility_Name['Sterling Care South Mountain'] = '21713';
+$Facility_Name['Summit Park Health & Rehabilitation Center'] = '21228';
+$Facility_Name['Sunrise of Columbia'] = '21044';
+$Facility_Name['Sunrise Senior Living Bethesda'] = '20814';
+$Facility_Name['The Charleston Senior Community'] = '20603';
+$Facility_Name['The Neighborhoods at St. Elizabeth Rehabilitation & Nursing Center'] = '21227';
+$Facility_Name['The Peartree House Assisted Living'] = '21122';
+$Facility_Name['The Resort at Chester Manor'] = '21620';
+$Facility_Name['Tribute at Black Hill'] = '20874';
+$Facility_Name['Tudor Heights Assisted Living'] = '21208';
+$Facility_Name['Village at Augsburg -- Nursing Home'] = '21207';
+$Facility_Name['Village at Rockville'] = '20850';
+$Facility_Name['Westgate Hills Rehabilitation & Healthcare Center'] = '21229';
+$Facility_Name['Westminster Healthcare Center'] = '21157';
 
+function cleanup($str){
+   $str = str_replace("'",'',$str);
+   $str = str_replace(".",'',$str);
+   $str = str_replace("&",'',$str);
+   $str = str_replace("/",'',$str);
+   $str = str_replace("(",'',$str);
+   $str = str_replace(")",'',$str); 
+   $str = str_replace("'",'',$str);
+   return $str; 
+}
+
+
+function coronavirus_Facility($Facility_Name,$date,$count){
+	// the order we call the function will matter...
+	global $core;
+	$q = "select * from coronavirus_facility where Facility_Name = '$Facility_Name' and report_date = '$date'";
+	$r = $core->query($q);
+	$d = mysqli_fetch_array($r);
+	// look for yesterday
+	$date2 = date('Y-m-d',strtotime($date)-86400);
+	$q2 = "select * from coronavirus_facility where Facility_Name = '$Facility_Name' and report_date = '$date2'";
+	$r2 = $core->query($q2);
+	$d2 = mysqli_fetch_array($r2);
+	if ($d2['id'] != ''){
+		// Let's Process Trend Data
+		$last_trend_direction = $d2['trend_direction'];
+		$last_trend_duration = $d2['trend_duration'];
+		$last_report_count = $d2['report_count'];
+		if ($count == $last_report_count){
+			$current_trend = 'FLAT';	
+		}elseif ($count > $last_report_count){
+			$current_trend = 'UP';	
+		}else{
+			$current_trend = 'DOWN';
+		}
+		if ($last_trend_direction == $current_trend){
+			$current_duration = $last_trend_duration + 1;
+		}else{
+			$current_duration = 0;
+		}
+	}else{
+		// we reached the start of data collection.	
+	}
+	
+	if ($d['id'] == ''){
+		$q = "insert into coronavirus_facility (Facility_Name,report_date,report_count,town_name,state_name,trend_direction,trend_duration) values ('$Facility_Name','$date','$count','$town','Maryland','$current_trend','$current_duration') ";
+	}else{
+		$q = "update coronavirus_facility set report_count = '$count', trend_direction = '$current_trend', trend_duration = '$current_duration'  where Facility_Name = '$Facility_Name' and report_date = '$date' ";
+		
+	}
+	$core->query($q);
+	slack_general("$q",'covid19-sql');
+
+}
+
+
+
+
+
+//coronavirus_Facility($Facility_Name,$date,$count);
 
 ?>
