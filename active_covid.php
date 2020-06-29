e7d5d0fa05e4259677a2948c8a6d3f06ceec21e2
Patrick McGuire <insidenothing@gmail.com>
2020-06-29 10:38:58 -0400

    Update active_covid.php

--- active_covid.php
+++ active_covid.php
@@ -11,10 +11,6 @@ while($d = mysqli_fetch_array($r)){
 	$zipcode[$zip] = $d['town_name'];
 }
 $date = $global_date;
-
-
-
-
 ob_start();
 echo "<h3>This list of ZIP codes have active cases. Cases are removed after 14 days.</h3><ol>";
 $q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date'  order by active_count DESC";
@@ -24,11 +20,11 @@ while ($d = mysqli_fetch_array($r)){
   $zip_c = $d['zip_code'];
   $name = $zipcode[$zip_c];
   $total = $total + $d['active_count'];
-  echo "<li><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_count']." infections. 30 Day Change ".$d['day30change_percentage']."% ".$d['percentage_direction30']."</li>";
+  echo "<li class='".$d['percentage_direction30']."'><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_count']." infections. 30 Day Change ".$d['day30change_percentage']."% ".$d['percentage_direction30']."</li>";
 }
 echo "</ol>";
 $list = ob_get_clean();
 
-echo "<h1>".number_format($total)." Maryland Active COVID-19 Infections for $date</h1>".$list;
+echo "<style> .up { background-color: yellow; } </style><h1>".number_format($total)." Maryland Active COVID-19 Infections for $date</h1>".$list;
 
 include_once('footer.php');
