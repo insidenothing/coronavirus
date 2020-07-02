e1d548cc3afb0c9d602e46c6df73afeda8e907e8
Patrick McGuire <insidenothing@gmail.com>
2020-07-02 11:53:10 -0400

    Update xlsx_data_api.php

--- xlsx_data_api.php
+++ xlsx_data_api.php
@@ -6,10 +6,20 @@ include_once('menu.php');
 function getPage($url){
     $curl = curl_init();
     curl_setopt ($curl, CURLOPT_URL, $url);
+    curl_setopt ($curl, CURLOPT_VERBOSE, true);
     curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire Coronavirus Monitor covid19math.net /%d.0",rand(4,50)));
     curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
-    $html = curl_exec ($curl);
+    $verbose = fopen('php://temp', 'w+');
+    curl_setopt ($curl, CURLOPT_STDERR, $verbose);
+    $html = curl_exec ($curl);  
+    if ($result === FALSE) {
+           printf("cUrl error (#%d): %s<br>\n", curl_errno($curl),
+           htmlspecialchars(curl_error($curl)));
+    }
+    rewind($verbose);
+    $verboseLog = stream_get_contents($verbose);
+    slack_general($verboseLog,'covid19-apis');
     curl_close ($curl);
     return $html;
 }
