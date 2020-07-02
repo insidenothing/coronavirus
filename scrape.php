a6ae735dc2f5161c171a44842d082a3f5b80d0b5
Patrick McGuire <insidenothing@gmail.com>
2020-07-02 16:35:12 -0400

    Update scrape.php

--- scrape.php
+++ scrape.php
@@ -2,7 +2,7 @@
 include_once('menu.php');
 
 
-function getPage($url){
+function getPage44($url){
     $curl = curl_init();
     curl_setopt ($curl, CURLOPT_URL, $url);
     curl_setopt ($curl, CURLOPT_VERBOSE, true);
@@ -25,8 +25,8 @@ function getPage($url){
 }
 
 
-$url = 'https://public.tableau.com/profile/population.health.dhec#!/vizhome/ZipCodeCountyLevel/CountyOption1';
-    $str = getPage($url);
+    $url = 'https://public.tableau.com/profile/population.health.dhec#!/vizhome/ZipCodeCountyLevel/CountyOption1';
+    $str = getPage44($url);
     echo $str;
 
 die();
