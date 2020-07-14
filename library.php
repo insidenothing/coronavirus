47e742ee691d8e58ba55d2b9a35f7af09a4f6e11
Patrick McGuire <insidenothing@gmail.com>
2020-07-14 16:26:09 -0400

    Update library.php

--- library.php
+++ library.php
@@ -5,7 +5,7 @@ include_once('menu.php');
 
 
 
-echo '<div class="row"><p>';
+echo '<div class="row">';
 echo "<h1>National Library of COVID API's</h1>";
 $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_order DESC, last_updated DESC ";
 $r = $core->query($q);
@@ -13,13 +13,13 @@ $r = $core->query($q);
 while($d = mysqli_fetch_array($r)){
   echo "<a class='btn btn-primary' data-toggle='collapse' href='#multiCollapseExample$d[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExample$d[id]'>$d[api_name]</a>";
 }
-echo "</p></div>";
+echo "</div>";
 
 
 
 $raws='';
 
-echo '<div class="row"><p>';
+echo '<div class="row">';
 $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_order DESC, last_updated DESC ";
 $r = $core->query($q);
 while($d = mysqli_fetch_array($r)){
@@ -33,27 +33,26 @@ while($d = mysqli_fetch_array($r)){
   if (substr($d['last_updated'],0,10) == date('Y-m-d',strtotime('-1 day'))){
     $color='lightyellow';
   }
-  echo "<p><a class='btn btn-info'>$d[api_name]</a></p>";
- // echo "<h3 style='background-color:$color;' title='$d[api_description]'>$d[run_order]: $d[last_updated] <u>$d[api_name]</u> $d[api_status]</h3>";
+  echo "<center><a class='btn btn-info'>$d[api_name]</a></center>";
   $url = $d['api_url'];
   $id = $d['id'];
   $name = $d['api_name'];
   $r2 = $core->query("SELECT id, cache_date_time, raw_response FROM coronavirus_api_cache where api_id = '$id' order by id DESC");
-  //$raws .= '<div id="list-example2" class="list-group2">';
   while($d2 = mysqli_fetch_array($r2)){
     echo "<a class='btn btn-warning' data-toggle='collapse' href='#multiCollapseExamplecache$d2[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExamplecache$d2[id]'>$d2[cache_date_time]</a>";
     $raws .= '<div class="col"><div class="collapse multi-collapse" id="multiCollapseExamplecache'.$d2['id'].'"><div class="card card-body"><h3>'.$d['api_name'].'</h3><pre><code>'.$d2[raw_response].'</code></pre></div></div></div>';
   }
- // $raws = "</div>";
   echo "</div></div></div>";
 }
-echo "</p></div>";
+echo "</div>";
 
 
 
-echo '<div class="row"><p>';
+echo '<div class="row">';
 echo $raws;
-echo "</p></div>";
+echo "</div>";
+
+
 /*
 if (isset($_GET['cache'])){
   echo '<div class="row">';
