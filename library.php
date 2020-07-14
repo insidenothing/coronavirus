390769641b5db9c4cf1f86024d2c6e5211bbfeaf
Patrick McGuire <insidenothing@gmail.com>
2020-07-14 16:27:04 -0400

    Update library.php

--- library.php
+++ library.php
@@ -11,7 +11,8 @@ $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_or
 $r = $core->query($q);
 
 while($d = mysqli_fetch_array($r)){
-  echo "<a class='btn btn-primary' data-toggle='collapse' href='#multiCollapseExample$d[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExample$d[id]'>$d[api_name]</a>";
+  echo "
+  <a class='btn btn-primary' data-toggle='collapse' href='#multiCollapseExample$d[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExample$d[id]'>$d[api_name]</a>";
 }
 echo "</div>";
 
@@ -39,8 +40,10 @@ while($d = mysqli_fetch_array($r)){
   $name = $d['api_name'];
   $r2 = $core->query("SELECT id, cache_date_time, raw_response FROM coronavirus_api_cache where api_id = '$id' order by id DESC");
   while($d2 = mysqli_fetch_array($r2)){
-    echo "<a class='btn btn-warning' data-toggle='collapse' href='#multiCollapseExamplecache$d2[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExamplecache$d2[id]'>$d2[cache_date_time]</a>";
-    $raws .= '<div class="col"><div class="collapse multi-collapse" id="multiCollapseExamplecache'.$d2['id'].'"><div class="card card-body"><h3>'.$d['api_name'].'</h3><pre><code>'.$d2[raw_response].'</code></pre></div></div></div>';
+    echo "
+    <a class='btn btn-warning' data-toggle='collapse' href='#multiCollapseExamplecache$d2[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExamplecache$d2[id]'>$d2[cache_date_time]</a>";
+    $raws .= '
+    <div class="col"><div class="collapse multi-collapse" id="multiCollapseExamplecache'.$d2['id'].'"><div class="card card-body"><h3>'.$d['api_name'].'</h3><pre><code>'.$d2[raw_response].'</code></pre></div></div></div>';
   }
   echo "</div></div></div>";
 }
