73bfd29f53b76e4c17eeed87648bd0b356035069
Patrick McGuire <insidenothing@gmail.com>
2020-07-14 15:26:22 -0400

    Update library.php

--- library.php
+++ library.php
@@ -3,7 +3,18 @@ $page_description = "National Library of COVID-19 API Data";
 include_once('menu.php');
 
 
-$id=0;
+
+echo "<h1>National Library of COVID API's</h1>";
+
+echo "<p>";
+$q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_order DESC, last_updated DESC ";
+$r = $core->query($q);
+while($d = mysqli_fetch_array($r)){
+  echo "<a class='btn btn-primary' data-toggle='collapse' href='#multiCollapseExample$d[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExample$d[id]'>$d[api_name]</a>";
+}
+echo "</p>";
+
+
 if (isset($_GET['cache'])){
   $id = substr(intval($_GET['cache']),0,10); // take int val # or 0 , limit to first 10 digits
   $q = "SELECT raw_response FROM coronavirus_api_cache where id = '$id' ";
@@ -13,10 +24,11 @@ if (isset($_GET['cache'])){
 }
 
 
-echo "<h1>National Library of COVID API's</h1><table><tr>";
+
 $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_order DESC, last_updated DESC ";
 $r = $core->query($q);
 while($d = mysqli_fetch_array($r)){
+  echo '<div class="row"><div class="col"><div class="collapse multi-collapse" id="multiCollapseExample'.$d['id'].'"><div class="card card-body">';
   //slack_general("$d[run_delay] second delay to check $d[api_name]",'covid19-apis');
   //sleep($d['run_delay']);
   $color = 'lightblue';
@@ -34,9 +46,9 @@ while($d = mysqli_fetch_array($r)){
   while($d2 = mysqli_fetch_array($r2)){
     echo "<li style='white-space:pre;'>$d2[id]: <a target='_Blank' href='?cache=$d2[id]'>$d2[cache_date_time]</a></li>";
   }
-  echo "</ol></td>";
+  echo "</div></div></div>";
 }
-echo "</tr></table>";
+//echo "</tr></table>";
 
 include_once('footer.php');
 ?>
