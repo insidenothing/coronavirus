fa8d2bb76c1919f8b51645b8a723929aed53dc5e
Patrick McGuire <insidenothing@gmail.com>
2020-07-14 16:20:31 -0400

    Update library.php

--- library.php
+++ library.php
@@ -11,7 +11,7 @@ $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_or
 $r = $core->query($q);
 
 while($d = mysqli_fetch_array($r)){
-  echo "<a class='btn btn-primary' data-target='#list-example' data-toggle='collapse' href='#multiCollapseExample$d[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExample$d[id]'>$d[api_name]</a>";
+  echo "<a class='btn btn-primary' data-toggle='collapse' href='#multiCollapseExample$d[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExample$d[id]'>$d[api_name]</a>";
 }
 echo "</p></div>";
 
@@ -23,7 +23,6 @@ echo '<div class="row"><p>';
 $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_order DESC, last_updated DESC ";
 $r = $core->query($q);
 while($d = mysqli_fetch_array($r)){
-  echo '<div id="list-example" class="list-group">';
   echo '<div class="col"><div class="collapse multi-collapse" id="multiCollapseExample'.$d['id'].'"><div class="card card-body">';
   //slack_general("$d[run_delay] second delay to check $d[api_name]",'covid19-apis');
   //sleep($d['run_delay']);
@@ -40,14 +39,13 @@ while($d = mysqli_fetch_array($r)){
   $id = $d['id'];
   $name = $d['api_name'];
   $r2 = $core->query("SELECT id, cache_date_time, raw_response FROM coronavirus_api_cache where api_id = '$id' order by id DESC");
-  $raws .= '<div id="list-example2" class="list-group2">';
+  //$raws .= '<div id="list-example2" class="list-group2">';
   while($d2 = mysqli_fetch_array($r2)){
-    echo "<a class='btn btn-warning' data-target='#list-example2' data-toggle='collapse' href='#multiCollapseExamplecache$d2[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExamplecache$d2[id]'>$d2[cache_date_time]</a>";
+    echo "<a class='btn btn-warning' data-toggle='collapse' href='#multiCollapseExamplecache$d2[id]' role='button' aria-expanded='false' aria-controls='multiCollapseExamplecache$d2[id]'>$d2[cache_date_time]</a>";
     $raws .= '<div class="col"><div class="collapse multi-collapse" id="multiCollapseExamplecache'.$d2['id'].'"><div class="card card-body"><h3>'.$d['api_name'].'</h3><pre><code>'.$d2[raw_response].'</code></pre></div></div></div>';
   }
-  $raws = "</div>";
+ // $raws = "</div>";
   echo "</div></div></div>";
-  echo "<div>";
 }
 echo "</p></div>";
 
