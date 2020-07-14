c024fd51012d34c2aea6a5cc09802f23e07f5db4
Patrick McGuire <insidenothing@gmail.com>
2020-07-14 15:02:56 -0400

    Update library.php

--- library.php
+++ library.php
@@ -6,13 +6,14 @@ include_once('menu.php');
 $id=0;
 if (isset($_GET['cache'])){
   $id = substr(intval($_GET['cache']),0,10); // take int val # or 0 , limit to first 10 digits
-  $q = "SELECT raw_response FROM coronavirus_api_cache where api_id = '$id' ";
+  $q = "SELECT raw_response FROM coronavirus_api_cache where id = '$id' ";
   $r = $core->query($q);
   $d = mysqli_fetch_array($r);
   echo "<div><h1>Response</h1><pre><code>$d[raw_response]</code></pre></div>";
 }
 
-echo "<div>";
+
+echo "<h1>Library</h1><table><tr>";
 $q = "SELECT * FROM coronavirus_apis where api_status = 'active' order by run_order DESC, last_updated DESC ";
 $r = $core->query($q);
 while($d = mysqli_fetch_array($r)){
@@ -25,16 +26,17 @@ while($d = mysqli_fetch_array($r)){
   if (substr($d['last_updated'],0,10) == date('Y-m-d',strtotime('-1 day'))){
     $color='lightyellow';
   }
-  echo "<li style='background-color:$color;' title='$d[api_description]'>$d[run_order]: $d[last_updated] <u>$d[api_name]</u> $d[api_status]</li>";
+  echo "<td valign='top'><li style='background-color:$color;' title='$d[api_description]'>$d[run_order]: $d[last_updated] <u>$d[api_name]</u> $d[api_status]</li><ol>";
   $url = $d['api_url'];
   $id = $d['id'];
   $name = $d['api_name'];
   $r2 = $core->query("SELECT id, cache_date_time FROM coronavirus_api_cache where api_id = '$id' order by id DESC");
   while($d2 = mysqli_fetch_array($r2)){
-    echo "<li>$d2[id]: $d2[cache_date_time] <a target='_Blank' href='?cache=$d2[id]'>SOURCE</a></li>";
+    echo "<li>$d2[id]: <a target='_Blank' href='?cache=$d2[id]'>$d2[cache_date_time]</a></li>";
   }
+  echo "</ol></td>";
 }
-echo "</div>";
+echo "</tr></table>";
 
 include_once('footer.php');
 ?>
