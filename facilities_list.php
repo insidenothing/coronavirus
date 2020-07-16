86a69245ed2da1ee08266078b5705532f4f574ab
Patrick McGuire <insidenothing@gmail.com>
2020-07-16 11:47:27 -0400

    show zip

--- facilities_list.php
+++ facilities_list.php
@@ -101,7 +101,7 @@ while ($d = mysqli_fetch_array($r)){
 		$remove_chart .=  '{ label: "'.$d['report_date'].'", y: '.$rolling.' }, ';
 	}
 	
-	$master_facility_table .= "<tr><td>$d[report_date]</td><td>$name</td><td>$Resident_Type</td><td>$d[report_count]</td><td>$d[Number_of_Resident_Cases]</td><td>$d[Number_of_Staff_Cases]</td><td>$d[Number_of_Resident_Deaths]</td><td>$d[Number_of_Staff_Deaths]</td></tr>";
+	$master_facility_table .= "<tr><td>$d[report_date]</td><td>$d[zip_code]</td><td>$name</td><td>$Resident_Type</td><td>$d[report_count]</td><td>$d[Number_of_Resident_Cases]</td><td>$d[Number_of_Staff_Cases]</td><td>$d[Number_of_Resident_Deaths]</td><td>$d[Number_of_Staff_Deaths]</td></tr>";
 	
 	
 	
@@ -310,6 +310,7 @@ var chartZIP<?PHP echo $i;?> = new CanvasJS.Chart("chartContainerZIP<?PHP echo $
 	<table>
 		<tr>
 			<td>Report Date</td>
+			<td>ZIP</td>
 			<td>Name</td>
 			<td>Resident Type</td>
 			<td>Report Count</td>
