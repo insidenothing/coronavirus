4b8cda6d4ef7a99fdadfd31299fad3271e6ec4b1
Patrick McGuire <insidenothing@gmail.com>
2020-07-20 14:07:05 -0400

    Create cache.php

---
+++ cache.php
@@ -0,0 +1,52 @@
+<?PHP
+if (isset($_GET['id'])){
+  if (isint($_GET['id'])){
+    $id = intval($_GET['id']);
+  }else{
+    die();
+  }
+}else{
+  die();
+}
+include_once('/var/www/secure.php'); // this makes $core with out credientals 
+$id = substr($id,0,10); // protect strlen
+$stmt = $core->prepare('SELECT * FROM coronavirus_api_cache WHERE id = ?');
+$stmt->bind_param('i', $id); // https://www.php.net/manual/en/mysqli-stmt.bind-param.php
+$stmt->execute();
+$result = $stmt->get_result();
+$cache = $result->fetch_assoc();
+$type = $cache['api_flavor'];
+if ($type == 'csv'){
+  $type = 'csv';
+  header('Content-Type: application/csv');
+  header('Content-Disposition: attachment; filename=covid19mathcache'.$id.'.csv');
+  header('Pragma: no-cache');
+}elseif($type == 'json'){
+  $type = 'json';
+  header('Content-Type: application/json');
+  header('Content-Disposition: attachment; filename=covid19mathcache'.$id.'.json');
+}elseif($type == 'xlsx'){
+  $type = 'xlsx';
+  header('Content-Disposition: attachment; filename=covid19mathcache'.$id.'.xlsx' );
+  header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  // header('Content-Length: ' . filesize($file)); 
+  // maybe get strlen of raw_response?
+  header('Content-Transfer-Encoding: binary');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+}elseif($type == 'zip-csv'){
+  header("Pragma: public");
+  header("Expires: 0");
+  header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+  header("Cache-Control: public");
+  header("Content-Description: File Transfer");
+  header("Content-type: application/octet-stream");
+  header("Content-Disposition: attachment; filename=covid19mathcache".$id.".zip");
+  header("Content-Transfer-Encoding: binary");
+  //header("Content-Length: ".filesize($filepath.$filename)); 
+}else{
+  // unknown type
+  die(); 
+}
+echo $cache['raw_response'];
+die(); // do nothing even if included
