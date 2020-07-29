e6e4ee514fe46b018f50369f074a2265bfec7795
Patrick McGuire <insidenothing@gmail.com>
2020-07-29 14:52:18 -0400

    Update facilities_table.php

--- Maryland/facilities_table.php
+++ Maryland/facilities_table.php
@@ -57,7 +57,7 @@ function make_chart2($range,$Facility_Name){
 	$your_date = strtotime($d['report_date']);
 	$datediff = $now - $your_date;
 	$days = round($datediff / (60 * 60 * 24));
-	  if ($i == 1 && $days < 14){
+	  if ($i == 1 && $days < 20){
 		// highlight and use as "latest data"
 		$master_facility_table .= "<tr style='background-color:yellow;'><td style='white-space:pre;'>$d[report_date] <b>$days days ago</b></td><td>$d[zip_code]</td><td>$name</td><td>$Resident_Type</td><td title='Total'>$d[report_count]</td><td title='Number_of_Resident_Cases'>$d[Number_of_Resident_Cases]</td><td title='Number_of_Staff_Cases'>$d[Number_of_Staff_Cases]</td><td title='Number_of_Resident_Deaths'>$d[Number_of_Resident_Deaths]</td><td title='Number_of_Staff_Deaths'>$d[Number_of_Staff_Deaths]</td></tr>";
     		$ReportCount = $ReportCount + $d['report_count'];
@@ -65,7 +65,7 @@ function make_chart2($range,$Facility_Name){
 		$NumberofStaffCases = $NumberofStaffCases + $d['Number_of_Staff_Cases'];
 		$NumberofResidentDeaths = $NumberofResidentDeaths + $d['Number_of_Resident_Deaths'];
 		$NumberofStaffDeaths = $NumberofStaffDeaths + $d['Number_of_Staff_Deaths'];
-	  }elseif ($i == 2 && $days < 30){
+	  }elseif ($i == 2 && $days < 40){
 		// highlight for delta
 		$master_facility_table .= "<tr style='background-color:lightblue;'><td style='white-space:pre;'>$d[report_date] <b>$days days ago</b></td><td>$d[zip_code]</td><td>$name</td><td>$Resident_Type</td><td title='Total'>$d[report_count]</td><td title='Number_of_Resident_Cases'>$d[Number_of_Resident_Cases]</td><td title='Number_of_Staff_Cases'>$d[Number_of_Staff_Cases]</td><td title='Number_of_Resident_Deaths'>$d[Number_of_Resident_Deaths]</td><td title='Number_of_Staff_Deaths'>$d[Number_of_Staff_Deaths]</td></tr>";
     		$ReportCount2 = $ReportCount2 + $d['report_count'];
