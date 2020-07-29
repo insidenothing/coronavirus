d4ff11151c000e6eda6c059901d00bbc25f185d4
Patrick McGuire <insidenothing@gmail.com>
2020-07-29 10:18:16 -0400

    convert to array

--- Maryland/facilities_abstract.php
+++ Maryland/facilities_abstract.php
@@ -11,8 +11,11 @@ while ($d = mysqli_fetch_array($r)){
   $r2 = $core->query($q2);
   $d2 = mysqli_fetch_array($r2);
   echo "<h1>$d[api_name] from $d2[cache_date_time]<h1>";
+  $json = $d2['raw_response'];
+  $array = json_decode($json, true);
   echo "<pre>";
-  echo $d2['raw_response'];
+  //echo $d2['raw_response'];
+  print_r($array);
   echo "</pre>";
 }
 $buffer=ob_get_clean();
