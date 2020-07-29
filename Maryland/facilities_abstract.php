2749fb88c548fd8e50ec00bff4f2cf4c75dcd017
Patrick McGuire <insidenothing@gmail.com>
2020-07-29 12:13:27 -0400

    Update facilities_abstract.php

--- Maryland/facilities_abstract.php
+++ Maryland/facilities_abstract.php
@@ -300,7 +300,12 @@ ob_start();
 $q = "select * from coronavirus_apis where run_order = '5000' "; // MD Facilities
 $r = $core->query($q);
 while ($d = mysqli_fetch_array($r)){
-  $q2 = "select * from coronavirus_api_cache where api_id = '$d[id]' order by id desc limit 0,1 ";
+	if ($_GET['date']){
+  		$q2 = "select * from coronavirus_api_cache where api_id = '$d[id]' and just_date = '$_GET[date]' order by id desc limit 0,1 ";
+	}else{
+		$q2 = "select * from coronavirus_api_cache where api_id = '$d[id]' order by id desc limit 0,1 ";
+	}
+	echo "<h3>$q2</h3>";
   $r2 = $core->query($q2);
   $d2 = mysqli_fetch_array($r2);
   echo "<h1>$d[api_name] from $d2[cache_date_time]</h1>";
