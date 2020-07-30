47c8567766a5a7ef65e7edf93dd4b5f97745f16c
Patrick McGuire <insidenothing@gmail.com>
2020-07-30 14:44:02 -0400

    Update community.php

--- community.php
+++ community.php
@@ -5,17 +5,14 @@ include_once('menu.php');
 
 <h1>Community Page - Facebook Login Required</h1>
 
-<script>
-$(document).ready(function(){
-    $("p").hide(5000);
-    $("a").hide(0);
-});
-</script>
+<div id='private'>
 
-<p><a href='https://www.google.com'>HIDDEN CONTENT</a> SPOILER is a paragraph with little content.</p>
+    <p><a href='https://www.google.com'>HIDDEN CONTENT</a> SPOILER is a paragraph with little content.</p>
 
-<p>This is another small paragraph.</p>
+    <p>This is another small paragraph.</p>
 
+</div>
+    
 <?PHP 
 include_once('footer.php');
 ?>
