9fa91acbad7a01d75cf82b229f18d0edef0ccc7a
Patrick McGuire <insidenothing@gmail.com>
2020-07-30 08:41:55 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -64,6 +64,30 @@ set_hits(); // internal page counter
 </script>
 <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"></script>
+
+	
+
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '964347477327114',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v7.0'
+    });
+      
+    FB.AppEvents.logPageView();   
+      
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>	
 	
 	
 <script type="text/javascript" src="/simpletreemenu.js">
