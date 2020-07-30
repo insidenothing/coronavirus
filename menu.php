181667ee1f712882ff0bf9a280ab8fd711fdef8b
Patrick McGuire <insidenothing@gmail.com>
2020-07-30 09:25:29 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -66,13 +66,56 @@ set_hits(); // internal page counter
 	
 	
 
-<script>
-function checkLoginState() {
-  FB.getLoginStatus(function(response) {
-    statusChangeCallback(response);
-  });
-}	
+	<script>
+
+  function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
+    console.log('statusChangeCallback');
+    console.log(response);                   // The current login status of the person.
+    if (response.status === 'connected') {   // Logged into your webpage and Facebook.
+      testAPI();  
+    } else {                                 // Not logged into your webpage or we are unable to tell.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this webpage.';
+    }
+  }
+
+
+  function checkLoginState() {               // Called when a person is finished with the Login Button.
+    FB.getLoginStatus(function(response) {   // See the onlogin handler
+      statusChangeCallback(response);
+    });
+  }
+
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '{app-id}',
+      cookie     : true,                     // Enable cookies to allow the server to access the session.
+      xfbml      : true,                     // Parse social plugins on this webpage.
+      version    : '{api-version}'           // Use this Graph API version for this call.
+    });
+
+
+    FB.getLoginStatus(function(response) {   // Called after the JS SDK has been initialized.
+      statusChangeCallback(response);        // Returns the login status.
+    });
+  };
+ 
+  function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+
+</script>
 	
+	
+	
+<script>
+
   window.fbAsyncInit = function() {
     FB.init({
       appId      : '964347477327114',
@@ -323,10 +366,11 @@ ddtreemenu.createTree("treemenu1", true)
 		
 	
 	
-<fb:login-button 
-  scope="public_profile,email"
-  onlogin="checkLoginState();">
+
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
 </fb:login-button>
+<div id="status">
+</div>
 	
 	
 
