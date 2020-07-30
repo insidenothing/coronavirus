25e44d07a51ae1cd3febd580a49090a4c72c1d54
Patrick McGuire <insidenothing@gmail.com>
2020-07-30 14:10:21 -0400

    Create community.php

---
+++ community.php
@@ -0,0 +1,12 @@
+<?PHP
+$page_description = "COVID 19 Spike Monitor";
+include_once('menu.php');
+?>
+
+<div>IF SIGNED INTO FACEBOOK</div>
+
+<div>IF *NOT* SIGNED INTO FACEBOOK</div>
+
+<?PHP 
+include_once('footer.php');
+?>
