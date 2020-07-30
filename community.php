00646fa474067fa9dbefd97067c532e146ff7d11
Patrick McGuire <insidenothing@gmail.com>
2020-07-30 14:41:20 -0400

    Update community.php

--- community.php
+++ community.php
@@ -3,9 +3,18 @@ $page_description = "COVID 19 Spike Monitor";
 include_once('menu.php');
 ?>
 
-<div>IF SIGNED INTO FACEBOOK</div>
+<h1>Community Page - Facebook Login Required</h1>
 
-<div>IF *NOT* SIGNED INTO FACEBOOK</div>
+<script>
+$(document).ready(function(){
+    $("p").hide(5000);
+    $("a").hide(0);
+});
+</script>
+
+<p><a href='https://www.google.com'>HIDDEN CONTENT</a> SPOILER is a paragraph with little content.</p>
+
+<p>This is another small paragraph.</p>
 
 <?PHP 
 include_once('footer.php');
