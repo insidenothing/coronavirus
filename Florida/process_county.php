d6b8fea1299aec797e7df7be8296f02564d66c93
Patrick McGuire <insidenothing@gmail.com>
2020-08-03 09:02:24 -0400

    Update process_county.php

--- Florida/process_county.php
+++ Florida/process_county.php
@@ -27,7 +27,7 @@ while($d = mysqli_fetch_array($r)){
 }
 
 
-function coronavirus_county($zip,$date,$count){
+function coronavirus_county($zip,$date,$count$testing){
 	//return "<li>coronavirus_county($zip,$date,$count)</li>";
 	if ($count == 0){
 		echo "[skip - count too low $zip for $date]";
@@ -36,7 +36,7 @@ function coronavirus_county($zip,$date,$count){
 	global $covid_db;
 	global $zipcode;
 	$town = $zipcode[$zip];
-	$testing=0;
+	//$testing=0;
 	$q = "select * from coronavirus_county where county_name = '$zip' and report_date = '$date' and state_name = 'florida'";
 	$r = $covid_db->query($q);
 	$d = mysqli_fetch_array($r);
@@ -70,7 +70,7 @@ function coronavirus_county($zip,$date,$count){
 		$q = "insert into coronavirus_county (testing_count,county_name,report_date,report_count,town_name,state_name,trend_direction,trend_duration) values ('$testing','$zip','$date','$count','$town','florida','$current_trend','$current_duration') ";
 	}else{
 		echo "[update $zip $date $count]";
-		$q = "update coronavirus_county set testing = '$testing', report_count = '$count', trend_direction = '$current_trend', trend_duration = '$current_duration', town_name = '$town'  where county_name = '$zip' and report_date = '$date' ";	
+		$q = "update coronavirus_county set testing_count = '$testing', report_count = '$count', trend_direction = '$current_trend', trend_duration = '$current_duration', town_name = '$town'  where county_name = '$zip' and report_date = '$date' ";	
 	}
 	$covid_db->query($q) or die(mysqli_error($covid_db));
 	//slack_general("$q",'covid19-sql');
@@ -96,14 +96,15 @@ $pieces = json_decode($d['raw_response'], true);
  
 
   foreach ($pieces['features'] as $key => $value){
-	  	//$time = $value['attributes']['DATE'] / 1000;
-		//$date = date('Y-m-d',$time+14400);
-	  	//echo "<li>$date </li>";
+	//$time = $value['attributes']['DATE'] / 1000;
+	//$date = date('Y-m-d',$time+14400);
+	//echo "<li>$date </li>";
 	$name = $value['attributes']['COUNTYNAME'];
 	$count = $value['attributes']['CasesAll'];
+	$testing = $value['attributes']['T_Total_Res'];
 	if ($name != 'A State'){
-		echo "<li>coronavirus_county($name,$date,$count);</li>";
-		coronavirus_county($name,$date,$count);
+		echo "<li>coronavirus_county($name,$date,$count,$testing);</li>";
+		coronavirus_county($name,$date,$count,$testing);
 	}
   }
   
