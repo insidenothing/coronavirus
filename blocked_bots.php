fb47b969f490136621b856e5b19e86044ab2c6b3
Patrick McGuire <insidenothing@gmail.com>
2020-08-11 13:30:27 -0400

    Update blocked_bots.php

--- blocked_bots.php
+++ blocked_bots.php
@@ -1,12 +1,21 @@
 <?PHP
 // die on bot match
-$bot = $_SERVER['HTTP_USER_AGENT'];
-$pos = strpos($bot, 'SemrushBot');
-if ($pos !== false) {
-    die('Your Bot (SemrushBot) Has Been Blocked contact Patrick McGuire at baltimorehacker [at] gmail.com');
-} 
-$pos = strpos($bot, 'AhrefsBot');
-if ($pos !== false) {
-    die('Your Bot (AhrefsBot) Has Been Blocked contact Patrick McGuire at baltimorehacker [at] gmail.com');
-} 
+if (isset($_SERVER['HTTP_USER_AGENT'];)){
+    
+    $bot = $_SERVER['HTTP_USER_AGENT'];
+    $pos = strpos($bot, 'SemrushBot');
+    if ($pos !== false) {
+        die('Your Bot (SemrushBot) Has Been Blocked contact Patrick McGuire at baltimorehacker [at] gmail.com');
+    } 
+    
+    $pos = strpos($bot, 'AhrefsBot');
+    if ($pos !== false) {
+        die('Your Bot (AhrefsBot) Has Been Blocked contact Patrick McGuire at baltimorehacker [at] gmail.com');
+    } 
+    
+}else{
+    
+  die('Your Bot (MISSING HTTP_USER_AGENT) Has Been Blocked contact Patrick McGuire at baltimorehacker [at] gmail.com');  
+    
+}
 ?>
