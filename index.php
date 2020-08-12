3c1e8e781110fe9cc41e60aae09897fd08ad5d79
Patrick McGuire <insidenothing@gmail.com>
2020-08-12 10:34:37 -0400

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ include_once('menu.php');
 	$zip_closed=0;
 	?>
 	<div class="row">
-	  <div class="col-sm-12">
+	  <div class="col-sm-6">
 		  <h1>Phase One Reopen <?PHP echo $date;?></h1><p>The following zip codes are flat for over 2 weeks and ready to look at how to begin phase one.</p>
 		  <ol>  
 		  <?PHP
-		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration > '13' and (trend_direction = 'DOWN' or trend_direction = 'FLAT' ) order by report_count desc";
+		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration > '13' and (trend_direction = 'DOWN' or trend_direction = 'FLAT' ) order by trend_duration desc";
 		    $r = $covid_db->query($q);
 		    while($d = mysqli_fetch_array($r)){
 		       $zip_open++;
@@ -20,29 +20,28 @@ include_once('menu.php');
 		    ?>
 		  </ol>
 	  </div>
-	</div>
-	<div class="row">
-	  <div class="col-sm-12">
+	
+	  <div class="col-sm-6">
 		  <h1>Not Phase One <?PHP echo $date;?></h1>
 		  <ol> 
 		    <?PHP
-		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration > '13' and trend_direction = 'UP' order by report_count desc ";
+		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration > '13' and trend_direction = 'UP' order by trend_duration desc ";
 		    $r = $covid_db->query($q);
 		    while($d = mysqli_fetch_array($r)){
 			    $zip_closed++;
-		       echo "<li><a href='zipcode.php?zip=$d[zip_code]'>$d[state_name] $d[zip_code] $d[trend_duration] days $d[trend_direction]  at $d[report_count]</a></li>"; 
+		       echo "<li><a href='zipcode.php?zip=$d[zip_code]'>$d[state_name] $d[zip_code] $d[trend_duration] days $d[trend_direction] at $d[report_count]</a></li>"; 
 		    }
-		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration = '13' order by report_count desc";
+		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration = '13' order by trend_duration desc";
 		    $r = $covid_db->query($q);
 		    while($d = mysqli_fetch_array($r)){
 			    $zip_closed++;
-		       echo "<li><a href='zipcode.php?zip=$d[zip_code]'>$d[state_name] $d[zip_code] $d[trend_duration] days $d[trend_direction]  at $d[report_count]</a></li>"; 
+		       echo "<li><a href='zipcode.php?zip=$d[zip_code]'>$d[state_name] $d[zip_code] $d[trend_duration] days $d[trend_direction] at $d[report_count]</a></li>"; 
 		    }
-		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration < '13' order by report_count desc";
+		    $q = "SELECT * FROM coronavirus_zip where report_date = '$date' and trend_duration < '13' order by trend_duration desc";
 		    $r = $covid_db->query($q);
 		    while($d = mysqli_fetch_array($r)){
 			    $zip_closed++;
-		       echo "<li><a href='zipcode.php?zip=$d[zip_code]'>$d[state_name] $d[zip_code] $d[trend_duration] days $d[trend_direction]  at $d[report_count]</a></li>"; 
+		       echo "<li><a href='zipcode.php?zip=$d[zip_code]'>$d[state_name] $d[zip_code] $d[trend_duration] days $d[trend_direction] at $d[report_count]</a></li>"; 
 		    }
 		    ?>
 		  </ol> 
