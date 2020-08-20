fd7b514f2e12a24ea18771ca0977b972bd2f6bc8
Patrick McGuire <insidenothing@gmail.com>
2020-08-20 10:39:24 -0400

    Update status.php

--- status.php
+++ status.php
@@ -3,10 +3,8 @@ $days_back = 45;
 $px = '25px';
 $page_description = "$days_back Day Data Acquisition Report";
 include_once('menu.php');
-
 $array=array();
-
-function check_zip($zip,$date){
+function check_zip($zip,$date,$api_id){
   global $covid_db;
   $q = "select id from coronavirus_zip where zip_code = '$zip' and report_date = '$date'";
   $r = $covid_db->query($q);
@@ -17,8 +15,7 @@ function check_zip($zip,$date){
    return "<span class='missing' title='$date'>☒</span>";
   }
 }
-
-function check_county($countyDOTstate,$date){
+function check_county($countyDOTstate,$date,$api_id){
   global $covid_db;
   $parts = explode('.',$countyDOTstate);
   $q = "select id from coronavirus_county where county_name = '$parts[0]' and state_name = '$parts[1]' and report_date = '$date'";
@@ -30,8 +27,7 @@ function check_county($countyDOTstate,$date){
    return "<span class='missing' title='$date'>☒</span>";
   }
 }
-
-function check_state($state,$date){
+function check_state($state,$date,$api_id){
   global $covid_db;
   $q = "select id from coronavirus_state where state_name = '$state' and report_date = '$date'";
   $r = $covid_db->query($q);
@@ -42,40 +38,20 @@ function check_state($state,$date){
    return "<span class='missing' title='$date'>☒</span>";
   }
 }
-
-/*
-// Deleware
-$array['state'][]  = 'Delaware';
-
-// NYC
-$array['zip'][]  = '11368';
-
-// Arizona Data Quality
-$array['zip'][] = '85283';
-
-// Maryland Data Quality
-$array['zip'][] = '21093'; // Maryland Zip
-$array['countyState'][] = 'Baltimore.Maryland'; // Maryland County
-
-// Florida Data Quality
-//$array['zip'][] = '33445'; // Old Florida Zip
-$array['zip'][] = 'Palm Beach-33445'; // New Florida Zip
-$array['countyState'][] = 'Palm Beach.Florida'; // Florida County
-
-// virginia Data Quality
-$array['zip'][] = '23462'; // virginia Zip
-$array['countyState'][] = 'Arlington.Virginia'; // virginia County
-
-*/
-
-
-
-
+function check_facility($name,$state,$date,$api_id){
+  global $covid_db;
+  $q = "select id from coronavirus_facility where Facility_Name = '$name' and state_name = '$state' and report_date = '$date'";
+  $r = $covid_db->query($q);
+  $d = mysqli_fetch_array($r);
+  if ($d['id'] > 0){
+   return "<span class='found' title='$date'>☑</span>"; 
+  }else{
+   return "<span class='missing' title='$date'>☒</span>";
+  }
+  
+}
 echo "<style> span { font-size: $px; font-weight:bold; } .found { background-color: green; } .missing { background-color: red; } .cache { background-color: orange; } </style><table>";
 echo "<tr><td colspan='4'><h1>Data 'Cache and Load' Status</h1></td></tr>";
-
-
-
 $q = "SELECT * FROM coronavirus_api_qc order by state_name, data_type ";
 $r = $covid_db->query($q);
 while ($d = mysqli_fetch_array($r)){
@@ -83,16 +59,17 @@ while ($d = mysqli_fetch_array($r)){
   for($i = $days_back; $i > -1; $i--){
     $date = date("Y-m-d", strtotime("-$i days"));
     if ($d['data_type'] == 'zip'){
-      echo check_zip($d['data_to_check'],$date);
+      echo check_zip($d['data_to_check'],$date,$d['api_id']);
     }elseif($d['data_type'] == 'county'){
-      echo check_county($d['data_to_check'].'.'.$d['state_name'],$date);
+      echo check_county($d['data_to_check'].'.'.$d['state_name'],$date,$d['api_id']);
     }elseif($d['data_type'] == 'state'){
-      echo check_state($d['data_to_check'],$date);
+      echo check_state($d['data_to_check'],$date,$d['api_id']);
+    }elseif($d['data_type'] == 'facility'){
+     check_facility($d['data_to_check'],$d['state_name'],$date,$d['api_id']); 
     }
   }
   echo "</td><tr>";
 }
 echo "</table>";
-
 include_once('footer.php');
 ?>
