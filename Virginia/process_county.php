17373f5c2d9c7fe7d4c3a2fcf6a6b80e95ae0e4d
Patrick McGuire <insidenothing@gmail.com>
2020-08-21 10:45:53 -0400

    Update process_county.php

--- Virginia/process_county.php
+++ Virginia/process_county.php
@@ -166,4 +166,5 @@ if (isset($_GET['id'])){
 	$q = "update coronavirus_county set change_percentage_time = '' where state_name = 'virginia' ";
 	$covid_db->query($q);	
 }
+$covid_db->query("update coronavirus_apis set last_run_date = NOW() where id = '35' ");
 die('DONE '.$q);
