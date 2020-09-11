c6f8f277892cb4cacac5f0635b99cf39678944c5
Patrick McGuire <insidenothing@gmail.com>
2020-09-11 12:24:33 -0400

    Update apis.php

--- apis.php
+++ apis.php
@@ -110,7 +110,7 @@ if ($_GET['run']){
               $cache_id = $covid_db->insert_id;
               $covid_db->query("update coronavirus_apis set last_updated = NOW() where id = '$id' ");
               if ($d['send_alert'] == 'yes'){
-                  $result_diff = preg_replace("/[^a-zA-Z0-9]+/", "", $diff);
+                  $result_diff = preg_replace("/[^a-zA-Z0-9]+/", " ", $diff);
                   slack_bypass("$left): *$name* (( $result_diff ))",'covid19-apis-update');
               }
               galert_mail('trigger@applet.ifttt.com',$name.' '.substr($result_diff,0,50).'...more','https://www.covid19math.net/diff.php?id='.$cache_id.'&id2='.$d2['id']);
