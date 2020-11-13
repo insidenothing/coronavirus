ebeb2aba7ed8ad99efa0c3d179e319a2e70b6edc
Patrick McGuire <insidenothing@gmail.com>
2020-11-13 16:02:10 -0500

    Update zip.php

--- Maryland/zip.php
+++ Maryland/zip.php
@@ -94,7 +94,7 @@ if(isset($_GET['id'])){
 */
 $array = json_decode($d['raw_response'], true);
 
-$zipData = $array['features'][0];
+$zipData = $array['features'];
 
 
 asort($zipData); // Sort Array (Ascending Order), According to Value - asort()
