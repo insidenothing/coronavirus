032046aac374797e158d2306ba9de67ae2d86f36
Patrick McGuire <insidenothing@gmail.com>
2020-11-13 16:28:49 -0500

    Update zip.php

--- Maryland/zip.php
+++ Maryland/zip.php
@@ -118,7 +118,7 @@ foreach ($zipData as $key => $value){
   // coronavirus_zip($zip,$date,$count);
 	$i = $i - 1;
 	//total11_03_2020
-	echo "<li>$key $value</li>";
+	echo "<li>$key ".$value['ZIP_CODE']." ".$value['total11_03_2020']."</li>";
 	//echo "<h1>$i : ".$zipcode[$key]." $key $date ".intval($value)." </h1>";
 	$count = intval($value);
 	if ($count == 0){
