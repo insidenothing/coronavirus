b9505f2c1422efce576d23eef3ad15f6e57e19b0
Patrick McGuire <insidenothing@gmail.com>
2020-11-21 14:52:27 -0500

    active_count

--- outbreak.php
+++ outbreak.php
@@ -14,7 +14,7 @@ while($d = mysqli_fetch_array($r)){
 $date = $global_date;
 echo "<h1>Outbreak Monitor for $date</h1>";
 echo "<h3>This table of ZIP codes are seeing a percentage increase in cases at 7, 14, 30, and 45 days.</h3><table width='100%' border='1' cellpadding='10' cellspacing='0'>";
-$q = "SELECT * FROM coronavirus_zip where change_percentage_time <> '00:00:00' and report_date = '$date' and percentage_direction = 'up' and percentage_direction14 = 'up' and percentage_direction30 = 'up' and percentage_direction45 = 'up' order by report_count DESC";
+$q = "SELECT * FROM coronavirus_zip where change_percentage_time <> '00:00:00' and report_date = '$date' and percentage_direction = 'up' and percentage_direction14 = 'up' and percentage_direction30 = 'up' and percentage_direction45 = 'up' order by active_count DESC";
 $r = $covid_db->query($q);
 while ($d = mysqli_fetch_array($r)){
   $zip_c = $d['zip_code'];
@@ -26,7 +26,7 @@ while ($d = mysqli_fetch_array($r)){
 echo "</table>";
 echo "<h1>Outbreak Monitor for $date</h1>";
 echo "<h3>This list of ZIP codes are seeing a percentage increase in cases at 7, 14, 30, and 45 days.</h3><ol>";
-$q = "SELECT * FROM coronavirus_zip where change_percentage_time <> '00:00:00' and report_date = '$date' and percentage_direction = 'up' and percentage_direction14 = 'up' and percentage_direction30 = 'up' and percentage_direction45 = 'up' order by report_count DESC";
+$q = "SELECT * FROM coronavirus_zip where change_percentage_time <> '00:00:00' and report_date = '$date' and percentage_direction = 'up' and percentage_direction14 = 'up' and percentage_direction30 = 'up' and percentage_direction45 = 'up' order by active_count DESC";
 $r = $covid_db->query($q);
 while ($d = mysqli_fetch_array($r)){
   $zip_c = $d['zip_code'];
