4f1bd75fa7115023eaf0d976cd28324110148ab1
Patrick McGuire <insidenothing@gmail.com>
2020-12-08 10:47:06 -0500

    Create zip_early.php

---
+++ Maryland/zip_early.php
@@ -0,0 +1,167 @@
+<?PHP
+//ob_get_clean();
+include_once('../menu.php');
+if (isset($_GET['delete'])){
+	$delete = date('Y-m-d');
+	$covid_db->query(" delete from coronavirus_zip where report_date = '$delete' and state_name = 'Maryland'");
+	die('done');
+}
+
+global $zipcode;
+$zipcode = array();
+$q = "select distinct zip_code, town_name from coronavirus_zip where town_name <> ''";
+$r = $covid_db->query($q);
+while($d = mysqli_fetch_array($r)){
+	$zip = $d['zip_code'];
+	$zipcode[$zip] = $d['town_name'];
+}
+/*
+?>
+<script>
+function scrolldown() {
+  setTimeout(
+    function()
+    {
+      window.scrollTo(0,document.body.scrollHeight);
+      scrolldown();
+    }, 1000
+  )
+}
+
+scrolldown();
+	
+	
+</script>
+<?PHP
+*/
+function coronavirus_zip($zip,$date,$count){
+	global $zipcode;
+	$town = '';
+	if (isset($zipcode[$zip])){
+		$town = $zipcode[$zip];
+	}
+	// the order we call the function will matter...
+	global $covid_db;
+	$q = "select * from coronavirus_zip where zip_code = '$zip' and report_date = '$date'";
+	$r = $covid_db->query($q);
+	$d = mysqli_fetch_array($r);
+	if ($d['id'] == ''){
+		echo "[insert $zip $count]";
+		$q = "insert into coronavirus_zip (zip_code,report_date,report_count,town_name,state_name) values ('$zip','$date','$count','$town','Maryland') ";
+	}else{
+		echo "[update $zip $count]";
+		$q = "update coronavirus_zip set report_count = '$count'  where zip_code = '$zip' and report_date = '$date' ";
+		
+	}
+	$covid_db->query($q);
+
+
+}
+
+global $nocases;
+global $cases;
+global $zipData;
+global $date;
+
+//$q = "select * from coronavirus_api_cache where api_id = '13' order by id desc";
+
+
+if(isset($_GET['id'])){
+	$id = $_GET['id'];
+	$q = "select * from coronavirus_api_cache where id = '$id' order by id desc limit 0, 1"; // always get the latest from the cache
+}else{
+	$q = "select * from coronavirus_api_cache where api_id = '13' order by id desc limit 0, 1"; // always get the latest from the cache	
+}
+
+
+$r = $covid_db->query($q);
+echo "<h1>$q</h1>";
+$d = mysqli_fetch_array($r);
+echo "<h1>Cache ID $d[id] from $d[cache_date_time]</h1>";
+
+
+//echo $d['raw_response'];
+
+
+/*
+if(isset($_GET['id'])){
+	$date = date('Y-m-d',strtotime($d['cache_date_time']));
+	$date_formated = 'total'.$date_formated = date('m_d_Y',strtotime($date));
+	$zipData = make_maryland_array3($d['raw_response'],$date_formated);
+}else{
+	$zipData = make_maryland_array3($d['raw_response'],'');
+}
+*/
+$array = json_decode($d['raw_response'], true);
+
+$zipData = $array['features'];
+
+
+
+asort($zipData); // Sort Array (Ascending Order), According to Value - asort()
+
+
+
+echo "<pre>";
+print_r($zipData);
+echo "</pre>";
+
+//die('dev hold');
+
+
+if (empty($_GET['run'])){
+	die('set run=1');
+}
+
+
+if (isset($_GET['date'])){
+	$date = $_GET['date'];
+}else{
+	$date = date('Y-m-d');	
+}
+
+$i=count($zipData);
+foreach ($zipData as $key => $value){
+  // coronavirus_zip($zip,$date,$count);
+	$i = $i - 1;
+	//total11_03_2020
+	echo "<li>$key ".$value['attributes']['ZIP_CODE'].' ';
+	//print_r($value);
+	$date1 = date('m_d_Y', strtotime($date));
+	$date2 = date('Y-m-d', strtotime($date));
+	$part = 'total'.$date1;
+	echo ' '.$part.' '.$value['attributes'][$part]."</li>";
+	//echo "<h1>$i : ".$zipcode[$key]." $key $date ".intval($value)." </h1>";
+	$count = intval($value['attributes'][$part]);
+	if ($count == 0){
+		$count = 7; 
+		// since we get data starting at 8- but only zips with cases are in the data set, 
+		// so let's fix all the data and start with 1 less than the first number we get, 7
+	}
+	coronavirus_zip($value['attributes']['ZIP_CODE'],$date2,$count);
+}
+slack_general("*DONE*",'covid19');
+//$buffer = ob_get_clean();
+
+
+//die('dev hold');	
+
+$covid_db->query("update coronavirus_apis set last_run_date = NOW() where id = '13' ");
+
+if (isset($_GET['id'])){
+	$cache_id = $_GET['id'];
+	$r = $covid_db->query("SELECT id, cache_date_time FROM coronavirus_api_cache where api_id = '13' and id > '$cache_id' order by id limit 0,1");
+   	$d = mysqli_fetch_array($r);
+	if ($d['id']){
+		echo "<meta http-equiv=\"refresh\" content=\"1; url=https://www.covid19math.net/Maryland/zip.php?id=".$d['id']."&run=1&from_id=$cache_id\">";
+	}
+}else{
+	$q = "update coronavirus_county set change_percentage_time = '' where state_name = 'maryland' ";
+	$covid_db->query($q);	
+}
+
+
+
+//header('Location: https://www.covid19math.net/zipcode.php?zip=21208&auto=1&state=maryland');
+
+die('done');
