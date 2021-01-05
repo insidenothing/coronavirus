7b88be9f68f4146ee61420da1348ffe45b392e27
Patrick McGuire <insidenothing@gmail.com>
2021-01-05 13:27:48 -0500

    Update blocked_bots.php

--- blocked_bots.php
+++ blocked_bots.php
@@ -1,7 +1,15 @@
 <?PHP
 //header('HTTP/1.0 403 Forbidden');
 $block_list    = array();
-$youtube_promote_link = 'https://www.youtube.com/channel/UCPatyRpy7cwh5wrZBMV5DJQ';
+$youtube_promote_links = array();
+////   
+////// Add YouTube Links
+////
+$youtube_promote_links[] = 'https://www.youtube.com/channel/UCPatyRpy7cwh5wrZBMV5DJQ'; // channel Patrick McGuire
+$youtube_promote_links[] = 'https://www.youtube.com/channel/UC4HIYpyjcrS-V6c2xmIwQvg'; // channel 3D Printing
+$youtube_promote_links[] = 'https://www.youtube.com/channel/UCNAW4EIdHrtIWXm74kS4cIg'; // channel Model Trains
+$youtube_promote_links[] = 'https://www.youtube.com/channel/UCeIC-ux5H_Km5blR9cOnjWw';  // channel covid data
+$youtube_promote_links[] = 'https://www.youtube.com/channel/UCRhUMYbpikXZ5xHEdDPNhgA';  // channel news 
 ////   
 ////// Add IP addresses, User Agents, Bot Names, Keywords to Block Below
 ////
@@ -19,6 +27,10 @@ $block_list[]  = 'crawler';
 ////   
 ////// Do not edit below here
 ////
+// Pick a random channel
+$top_link_index = count($youtube_promote_links) - 1;
+$link_rand_index = rand(0,$top);
+$youtube_promote_link = $youtube_promote_links[$link_rand_index];
 // IP Addresses to Block 
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
    $host = $_SERVER['HTTP_CLIENT_IP'];
@@ -29,7 +41,7 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 }
 if (in_array($host, $block_list)) {
    $reason_blocked_message = 'IP blocked';
-   error_log($reason_blocked_message.' '.$ip."\n", 3, "/var/tmp/blocked_bots.log");
+   error_log($reason_blocked_message.' '.$ip." $youtube_promote_link \n", 3, "/var/tmp/blocked_bots.log");
    header('Location: '.$youtube_promote_link);
    die();
 } 
@@ -39,7 +51,7 @@ if (isset($_GET)){
         $pos = strpos($v, 'UNION');
         if ($pos !== false) {
            $reason_blocked_message = 'UNION blocked';
-           error_log($reason_blocked_message.' '.$v."\n", 3, "/var/tmp/blocked_bots.log");
+           error_log($reason_blocked_message.' '.$v." $youtube_promote_link \n", 3, "/var/tmp/blocked_bots.log");
            header('Location: '.$youtube_promote_link);
            die();
         } 
@@ -52,7 +64,7 @@ foreach ($block_list as $blocked){
        $pos = strpos($page, $blocked);
        if ($pos !== false) {
            $reason_blocked_message = $blocked.' blocked.';
-           error_log($reason_blocked_message.' '.$page."\n", 3, "/var/tmp/blocked_bots.log");
+           error_log($reason_blocked_message.' '.$page." $youtube_promote_link \n", 3, "/var/tmp/blocked_bots.log");
            header('Location: '.$youtube_promote_link);
            die();
        } 
@@ -65,14 +77,14 @@ if (isset($_SERVER['HTTP_USER_AGENT'])){
        $pos = strpos($bot, $blocked);
        if ($pos !== false) {
            $reason_blocked_message = $blocked.' blocked.';
-           error_log($reason_blocked_message.' '.$bot."\n", 3, "/var/tmp/blocked_bots.log");
+           error_log($reason_blocked_message.' '.$bot."$youtube_promote_link \n", 3, "/var/tmp/blocked_bots.log");
            header('Location: '.$youtube_promote_link);
            die();
        } 
     }   
 }else{
    $reason_blocked_message = 'Missing HTTP_USER_AGENT';
-   error_log($reason_blocked_message."\n", 3, "/var/tmp/blocked_bots.log");
+   error_log($reason_blocked_message." $youtube_promote_link \n", 3, "/var/tmp/blocked_bots.log");
    header('Location: '.$youtube_promote_link);
    die();
 }
