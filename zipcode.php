78e4d6f3a3138e8effb5ab86f7a75339ad408740
Patrick McGuire <insidenothing@gmail.com>
2021-01-06 11:21:58 -0500

    Update zipcode.php

--- zipcode.php
+++ zipcode.php
@@ -659,7 +659,7 @@ if (isset($_GET['auto']) && empty($_GET['state'])){
 	}
 }
 if (isset($_GET['auto']) && isset($_GET['state'])){
-	$q = "SELECT zip_code FROM coronavirus_zip where change_percentage_time = '00:00:00' and report_date = '$date' and zip_code <> '$zip' and state_name = '$_GET[state]' order by active_count DESC";
+	$q = "SELECT zip_code FROM coronavirus_zip where change_percentage_time = '00:00:00' and report_date = '$date' and zip_code <> '$zip' and state_name = '$_GET[state]' order by report_count DESC";
 	$r = $covid_db->query($q);
 	$d = mysqli_fetch_array($r);
 	$left = mysqli_num_rows($r);
