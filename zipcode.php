1bd00b4e28c19013890c81dd4eeb1a3de4e96755
Patrick McGuire <insidenothing@gmail.com>
2021-01-06 10:49:21 -0500

    Update zipcode.php

--- zipcode.php
+++ zipcode.php
@@ -664,7 +664,7 @@ if (isset($_GET['auto']) && isset($_GET['state'])){
 	$d = mysqli_fetch_array($r);
 	$left = mysqli_num_rows($r);
 	if ($left > 0){
-		echo "<meta http-equiv=\"refresh\" content=\"5; url=https://www.covid19math.net/zipcode.php?zip=".$d['zip_code']."&auto=$left&when=$date&state=$_GET[state]\">";
+		echo "<meta http-equiv=\"refresh\" content=\"3; url=https://www.covid19math.net/zipcode.php?zip=".$d['zip_code']."&auto=$left&when=$date&state=$_GET[state]\">";
 	}
 	$auto_message=" ($left)";
 }
