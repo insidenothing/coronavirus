dc488786294379d5f4e04eaaa2a7f93c64573744
Patrick McGuire <insidenothing@gmail.com>
2021-01-15 14:11:17 -0500

    Update active.php

--- Maryland/active.php
+++ Maryland/active.php
@@ -20,10 +20,10 @@ if (isset($_GET['sort'])){
 }
 
 
-echo "<a href='?sort=count'>Sort by Count</a><table><tr><td valign='top' width='33%'>";
+
 
 ob_start();
-echo "<h3>Cases are removed after 14 days and 28 days, sorted by $order.</h3><ol>";
+
 $q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date' and state_name = 'Maryland'  order by $order DESC";
 $r = $covid_db->query($q);
 $total = 0;
@@ -43,85 +43,6 @@ echo "</ol>";
 $list = ob_get_clean();
 
 echo "<h1>".number_format($total)." to ".number_format($total2)." Maryland Active COVID-19 Cases $date</h1><style> .up { background-color: yellow; font-weight:bold; } </style>".$list;
-
-echo "</td><td valign='top' width='25%'>";
-
-ob_start();
-echo "<h3>Cases are removed after 14 days and 28 days, sorted by $order.</h3><ol>";
-$q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date' and state_name = 'Virginia'  order by $order DESC";
-$r = $covid_db->query($q);
-$total = 0;
-$total2 = 0;
-while ($d = mysqli_fetch_array($r)){
-  $zip_c = $d['zip_code'];
-  $name = $zipcode[$zip_c];
-  $total = $total + $d['active_count'];
-	$total2 = $total2 + $d['active_count_28day'];
-	$to = '';
-	if ($d['active_count_28day'] > 0){
-	 $to = "to ".$d['active_count_28day'];	
-	}
-  echo "<li class='".$d['percentage_direction']."'><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_count']." $to infections. 7 Day Change ".$d['day7change_percentage']."% ".$d['percentage_direction']."</li>";
-}
-echo "</ol>";
-$list = ob_get_clean();
-
-echo "<h1>".number_format($total)." to ".number_format($total2)." Virginia Active COVID-19 Cases $date</h1><style> .up { background-color: yellow; font-weight:bold; } </style>".$list;
-
-echo "</td><td valign='top' width='25%'>";
-
-ob_start();
-echo "<h3>Cases are removed after 14 days and 28 days, sorted by $order.</h3><ol>";
-$q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date' and state_name = 'New York'  order by $order DESC";
-$r = $covid_db->query($q);
-$total = 0;
-$total2 = 0;
-while ($d = mysqli_fetch_array($r)){
-  $zip_c = $d['zip_code'];
-  $name = $zipcode[$zip_c];
-  $total = $total + $d['active_count'];
-	$total2 = $total2 + $d['active_count_28day'];
-	$to = '';
-	if ($d['active_count_28day'] > 0){
-	 $to = "to ".$d['active_count_28day'];	
-	}
-  echo "<li class='".$d['percentage_direction']."'><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_count']." $to infections. 7 Day Change ".$d['day7change_percentage']."% ".$d['percentage_direction']."</li>";
-}
-echo "</ol>";
-$list = ob_get_clean();
-
-echo "<h1>".number_format($total)." to ".number_format($total2)." New York City Active COVID-19 Cases $date</h1><style> .up { background-color: yellow; font-weight:bold; } </style>".$list;
-
-
-echo "</td><td valign='top' width='25%'>";
-
-ob_start();
-echo "<h3>Cases are removed after 14 days and 28 days, sorted by $order.</h3><ol>";
-$q = "SELECT * FROM coronavirus_zip where active_count > '0' and report_date = '$date' and state_name = 'Florida'  order by $order DESC";
-$r = $covid_db->query($q);
-$total = 0;
-$total2 = 0;
-while ($d = mysqli_fetch_array($r)){
-  $zip_c = $d['zip_code'];
-  $name = $zipcode[$zip_c];
-  $total = $total + $d['active_count'];
-	$total2 = $total2 + $d['active_count_28day'];
-	$to = '';
-	if ($d['active_count_28day'] > 0){
-	 $to = "to ".$d['active_count_28day'];	
-	}
-  echo "<li class='".$d['percentage_direction']."'><a href='zipcode.php?zip=".$d['zip_code']."'>".$d['zip_code']." $name ".$d['active_count']." $to infections. 7 Day Change ".$d['day7change_percentage']."% ".$d['percentage_direction']."</li>";
-}
-echo "</ol>";
-$list = ob_get_clean();
-
-echo "<h1>".number_format($total)." to ".number_format($total2)." Florida Active COVID-19 Cases $date</h1><style> .up { background-color: yellow; font-weight:bold; } </style>".$list;
-
-
-echo "</td>";
-
-
-echo "</tr></table>";
 
 
 include_once('../footer.php');
