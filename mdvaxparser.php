c8518f0218fc3f24e18a536df8c17fe5cf57e0c7
Patrick McGuire <insidenothing@gmail.com>
2021-03-08 17:36:55 -0500

    Update mdvaxparser.php

--- mdvaxparser.php
+++ mdvaxparser.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 require_once('/var/www/secure.php');
 //TwittterAPIkey
 //TwitterAPIsecretkey 
@@ -7,8 +11,14 @@ require_once('/var/www/secure.php');
 
 $filteredStream = \Spatie\TwitterLabs\FilteredStream\FilteredStreamFactory::create($TwittterAPIkey, $TwitterAPIsecretkey);
 
+
+
+
+/*
+
 $filteredStream->addRule(
     new \Spatie\TwitterLabs\FilteredStream\Rule('cat has:media', 'cat photos')
 );
 
 $filteredStream->onTweet(fn (Tweet $tweet) => print($tweet->text . PHP_EOL))->start();
+*/
