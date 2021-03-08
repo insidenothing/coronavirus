38213e4f12ddfea5aba441d1bedc5f9713eec893
Patrick McGuire <insidenothing@gmail.com>
2021-03-08 15:51:44 -0500

    Create mdvaxparser.php

---
+++ mdvaxparser.php
@@ -0,0 +1,20 @@
+<?PHP
+
+function getPage($url){
+    $curl = curl_init();
+    curl_setopt ($curl, CURLOPT_URL, $url);
+    curl_setopt ($curl, CURLOPT_USERAGENT, sprintf("McGuire Coronavirus Monitor covid19math.net /%d.0",rand(4,50)));
+    curl_setopt ($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt ($curl, CURLOPT_SSL_VERIFYPEER, 0);
+    $html = curl_exec ($curl);
+    curl_close ($curl);
+    return $html;
+}
+
+
+$feed = "https://twitter.com/mdvaxalerts";
+
+
+echo getPage($feed);
+
+
