242ab1fcd6630be2cf849fa6155650e3a8f3bdf2
Patrick McGuire <insidenothing@gmail.com>
2021-03-08 17:12:37 -0500

    Update mdvaxparser.php

--- mdvaxparser.php
+++ mdvaxparser.php
@@ -6,84 +6,13 @@ require_once('/var/www/secure.php');
 //TwitterAPIsecretkey 
 //TwitterBearertoken 
 
-// Step 2: Create an OAuth Signature
 
-$oauth_hash = '';
+$filteredStream = \Spatie\TwitterLabs\FilteredStream\FilteredStreamFactory::create($TwittterAPIkey, $TwitterAPIsecretkey);
 
-$oauth_hash .= 'oauth_consumer_key='.$TwittterAPIkey.'&';
+$filteredStream->addRule(
+    new \Spatie\TwitterLabs\FilteredStream\Rule('cat has:media', 'cat photos')
+);
 
-$oauth_hash .= 'oauth_nonce=' . time() . '&';
-
-$oauth_hash .= 'oauth_signature_method=HMAC-SHA1&';
-
-$oauth_hash .= 'oauth_timestamp=' . time() . '&';
-
-$oauth_hash .= 'oauth_token='.$TwitterBearertoken.'&';
-
-$oauth_hash .= 'oauth_version=1.0';
-
-$base = '';
-
-$base .= 'GET';
-
-$base .= '&';
-
-$base .= rawurlencode('https://api.twitter.com/1.1/statuses/user_timeline.json');
-
-$base .= '&';
-
-$base .= rawurlencode($oauth_hash);
-
-$key = '';
-
-$key .= rawurlencode($TwitterAPIsecretkey);
-
-$key .= '&';
-
-$key .= rawurlencode($TwitterBearertoken);
-
-$signature = base64_encode(hash_hmac('sha1', $base, $key, true));
-
-$signature = rawurlencode($signature);
-
-// Step 3: Construct the cURL Headers
-
-$oauth_header = '';
-
-$oauth_header .= 'oauth_consumer_key="'.$TwittterAPIkey.'", ';
-
-$oauth_header .= 'oauth_nonce="' . time() . '", ';
-
-$oauth_header .= 'oauth_signature="' . $signature . '", ';
-
-$oauth_header .= 'oauth_signature_method="HMAC-SHA1", ';
-
-$oauth_header .= 'oauth_timestamp="' . time() . '", ';
-
-$oauth_header .= 'oauth_token="'.$TwitterBearertoken.'", ';
-
-$oauth_header .= 'oauth_version="1.0", ';
-
-$curl_header = array("Authorization: Oauth {$oauth_header}", 'Expect:');
-
-// Step 4: Make the cURL Request
-
-$curl_request = curl_init();
-
-curl_setopt($curl_request, CURLOPT_HTTPHEADER, $curl_header);
-
-curl_setopt($curl_request, CURLOPT_HEADER, false);
-
-curl_setopt($curl_request, CURLOPT_URL, 'https://api.twitter.com/1.1/statuses/user_timeline.json');
-
-curl_setopt($curl_request, CURLOPT_RETURNTRANSFER, true);
-
-curl_setopt($curl_request, CURLOPT_SSL_VERIFYPEER, false);
-
-$json = curl_exec($curl_request);
-
-curl_close($curl_request);
-
-$array = json_decode($json, true);
-
-print_r($array);
+$filteredStream
+    ->onTweet(fn (Tweet $tweet) => print($tweet->text . PHP_EOL))
+    ->start();
